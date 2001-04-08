4b3698229c5249707cd8e259fbae293eb0920d64
Bob Kline <bkline@rksystems.com>
2001-04-08 22:57:41 +0000

    <R377> Added Unicode mapping.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/Glossary.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/Glossary.py
@@ -1,9 +1,12 @@
 #----------------------------------------------------------------------
-# $Id: Glossary.py,v 1.2 2001-03-27 21:12:36 bkline Exp $
+# $Id: Glossary.py,v 1.3 2001-04-08 22:57:41 bkline Exp $
 #
 # Displays CDR glossary term.
 # 
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2001/03/27 21:12:36  bkline
+# Fixed comment (no longer stub version); added RCS Log keyword.
+#
 #----------------------------------------------------------------------
 import cgi, cdrcgi, cdr
 
@@ -14,9 +17,9 @@ title   = "CDR Glossary"
 fields  = cgi.FieldStorage() or cdrcgi.bail("No Term Found", title)
 term    = fields.getvalue('Term') or cdrcgi.bail("No Term Found", title)
 session = cdr.login('rmk', '***REDACTED***') or cdrcgi.bail("Not Authorized", title)
-doc     = cdr.filterDoc(session, term, 'CDR0000190704')
+doc     = cdr.filterDoc(session, 'CDR0000190704', docId = term)
 
 #----------------------------------------------------------------------
 # Output the document.
 #----------------------------------------------------------------------
-cdrcgi.sendPage(doc)
+cdrcgi.sendPage(cdrcgi.decode(doc))
