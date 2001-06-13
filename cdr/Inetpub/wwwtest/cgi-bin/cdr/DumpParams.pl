d5c65dbedbcdb8a13301b6bce0f5e2790cfff6e5
Bob Kline <bkline@rksystems.com>
2001-06-13 22:35:02 +0000

    <R489> Initial revision

---
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/DumpParams.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use CGI;
+
+$query = new CGI;
+@keywords = $query->keywords;
+print "Content-type: text/html\n\n";
+print "<H2>Current Values</H2> $query\n<HR>\n";
+#print "<HTML><HEAD><TITLE>Parameter list</TITLE></HEAD><BODY>\n";
+#foreach $keyword (@keywords) { print "$keyword<BR>\n"; }
+print $query->dump;
+print "</BODY></HTML>\n";
+# @values = $query->param('foo');
