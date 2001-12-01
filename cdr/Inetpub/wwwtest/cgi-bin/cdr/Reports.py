80cdbee3140bfc9bc08de8567b7694c0419257d4
Bob Kline <bkline@rksystems.com>
2001-12-01 18:07:34 +0000

    <R854> Added some new reports.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/Reports.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/Reports.py
@@ -1,10 +1,13 @@
 #----------------------------------------------------------------------
 #
-# $Id: Reports.py,v 1.1 2001-06-13 22:16:32 bkline Exp $
+# $Id: Reports.py,v 1.2 2001-12-01 18:07:34 bkline Exp $
 #
 # Prototype for editing CDR linking tables.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2001/06/13 22:16:32  bkline
+# Initial revision
+#
 #
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, string
@@ -23,7 +26,11 @@ header  = cdrcgi.header(title, title, section, "", buttons)
 # Put up a stub page for now.
 #----------------------------------------------------------------------
 form = "<OL>\n"
-reports = [('CdrReport.py', 'Inactive Documents')]
+reports = [('CdrReport.py', 'Inactive Documents'),
+           ('UnchangedDocs.py', 'Unchanged Documents'),
+           ('CheckUrls.py', 'Inactive Hyperlinks'),
+           ('TermUsage.py', 'Term Usage'),
+           ('ModifiedPubMedDocs.py', 'Modified PubMed Documents')]
 if type(reports) == type(""): cdrcgi.bail(reports)
 
 for r in reports:
