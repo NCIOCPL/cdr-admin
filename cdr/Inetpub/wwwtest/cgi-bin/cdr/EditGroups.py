722b178e2798fdd7f3c4e3658109941f5cc0cd9f
Bob Kline <bkline@rksystems.com>
2002-02-20 23:59:44 +0000

    <R1365> Added button for returning to main Admin menu.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/EditGroups.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/EditGroups.py
@@ -1,23 +1,27 @@
 #----------------------------------------------------------------------
 #
-# $Id: EditGroups.py,v 1.1 2001-06-13 22:16:32 bkline Exp $
+# $Id: EditGroups.py,v 1.2 2002-02-20 23:59:44 bkline Exp $
 #
 # Prototype for editing CDR groups.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2001/06/13 22:16:32  bkline
+# Initial revision
+#
 #
 #----------------------------------------------------------------------
-import cgi, cdr, cdrcgi, re, string, urllib
+import cgi, cdr, cdrcgi, re, string, sys, urllib
 
 #----------------------------------------------------------------------
 # Set the form variables.
 #----------------------------------------------------------------------
 fields  = cgi.FieldStorage()
 session = cdrcgi.getSession(fields)
+action  = cdrcgi.getRequest(fields)
 title   = "CDR Administration"
 section = "Manage Groups"
-buttons = []
-header  = cdrcgi.header(title, title, section, "", buttons)
+buttons = ["Return to Admin Main Menu"]
+header  = cdrcgi.header(title, title, section, "EditGroups.py", buttons)
 
 #----------------------------------------------------------------------
 # Make sure the login was successful.
@@ -25,10 +29,22 @@ header  = cdrcgi.header(title, title, section, "", buttons)
 if not session: cdrcgi.bail('Unknown or expired CDR session.')
 
 #----------------------------------------------------------------------
+# Return to the main menu if requested.
+#----------------------------------------------------------------------
+if action == "Return to Admin Main Menu":
+    where = "http://%s%s/Admin.py?%s=%s" % (cdrcgi.WEBSERVER,
+                                            cdrcgi.BASE,
+                                            cdrcgi.SESSION,
+                                            session)
+    #cdrcgi.bail(where)
+    print "Location:%s\n" % where
+    sys.exit(0)
+
+#----------------------------------------------------------------------
 # Retrieve the list of groups from the server.
 #----------------------------------------------------------------------
 groups = cdr.getGroups(session)
-if type(groups) == type(""): cdrcgi.bail(groups)
+if type(groups) == type("") or type(groups) == type(u""): cdrcgi.bail(groups)
 
 #----------------------------------------------------------------------
 # Put up the main menu.
@@ -47,5 +63,6 @@ menu += """\
 """ % (cdrcgi.BASE, cdrcgi.SESSION, session, "ADD NEW GROUP",
        cdrcgi.BASE, cdrcgi.SESSION, session, "LOG OUT")
 menu += "</OL>"
-
-cdrcgi.sendPage(header + menu + "</BODY></HTML>")
+menu += "<INPUT TYPE='hidden' NAME='%s' VALUE='%s'>" % (cdrcgi.SESSION,
+                                                       session)
+cdrcgi.sendPage(header + menu + "</FORM></BODY></HTML>")
