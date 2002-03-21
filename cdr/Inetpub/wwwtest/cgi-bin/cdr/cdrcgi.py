b728ea2539994d27574283dff09d61dc40d9ade5
Bob Kline <bkline@rksystems.com>
2002-03-21 20:01:59 +0000

    <R1540> Added function for converting integers to Roman numerals.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/cdrcgi.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/cdrcgi.py
@@ -1,10 +1,13 @@
 #----------------------------------------------------------------------
 #
-# $Id: cdrcgi.py,v 1.8 2002-03-02 13:50:54 bkline Exp $
+# $Id: cdrcgi.py,v 1.9 2002-03-21 20:01:59 bkline Exp $
 #
 # Common routines for creating CDR web forms.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.8  2002/03/02 13:50:54  bkline
+# Added unicodeToLatin1().
+#
 # Revision 1.7  2002/02/21 15:21:08  bkline
 # Added navigateTo() function.
 #
@@ -693,3 +696,64 @@ def advancedSearchResultsPage(docType, rows, strings, filter):
 """ % (i + 1, cgi.escape(title, 1), dtcol, '/cgi-bin/cdr/Filter.py', 
        docId, filt, docId)
     return html + "  </TABLE>\n </BODY>\n</HTML>\n"
+
+#----------------------------------------------------------------------
+# Borrowed from ActiveState's online Python cookbook.
+#----------------------------------------------------------------------
+def int_to_roman(input):
+   """
+   Convert an integer to Roman numerals.
+
+   Examples:
+   >>> int_to_roman(0)
+   Traceback (most recent call last):
+   ValueError: Argument must be between 1 and 3999
+
+   >>> int_to_roman(-1)
+   Traceback (most recent call last):
+   ValueError: Argument must be between 1 and 3999
+
+   >>> int_to_roman(1.5)
+   Traceback (most recent call last):
+   TypeError: expected integer, got <type 'float'>
+
+   >>> for i in range(1, 21): print int_to_roman(i)
+   ...
+   I
+   II
+   III
+   IV
+   V
+   VI
+   VII
+   VIII
+   IX
+   X
+   XI
+   XII
+   XIII
+   XIV
+   XV
+   XVI
+   XVII
+   XVIII
+   XIX
+   XX
+   >>> print int_to_roman(2000)
+   MM
+   >>> print int_to_roman(1999)
+   MCMXCIX
+   """
+   if type(input) != type(1):
+      raise TypeError, "expected integer, got %s" % type(input)
+   if not 0 < input < 4000:
+      raise ValueError, "Argument must be between 1 and 3999"   
+   ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+   nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+   result = ""
+   for i in range(len(ints)):
+      count = int(input / ints[i])
+      result += nums[i] * count
+      input -= ints[i] * count
+   return result
+
