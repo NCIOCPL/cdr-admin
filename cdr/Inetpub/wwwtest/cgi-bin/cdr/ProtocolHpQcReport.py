7398bb807544adf993ff2313a1ce798a27fd70f1
Bob Kline <bkline@rksystems.com>
2002-05-17 21:15:40 +0000

    <R1846> Plugged in new filters from Cheryl.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/ProtocolHpQcReport.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/ProtocolHpQcReport.py
@@ -1,10 +1,13 @@
 #----------------------------------------------------------------------
 #
-# $Id: ProtocolHpQcReport.py,v 1.3 2002-05-08 17:41:52 bkline Exp $
+# $Id: ProtocolHpQcReport.py,v 1.4 2002-05-17 21:15:40 bkline Exp $
 #
 # Protocol Health Professional QC Report.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2002/05/08 17:41:52  bkline
+# Updated to reflect Volker's new filter names.
+#
 # Revision 1.2  2002/05/03 20:27:32  bkline
 # Changed filter name to match name change made by Cheryl.
 #
@@ -27,7 +30,9 @@ docId   = fields.getvalue(cdrcgi.DOCID) or cdrcgi.bail("No Document", title)
 #----------------------------------------------------------------------
 filters = [
     "name:Denormalization Filter (1/1): InScope Protocol",
-    "name:Health Professional Protocol QC Content Report"
+   #"name:Health Professional Protocol QC Content Report"
+    "name:Create InScope Protocol XML for Full Protocol QC Report",
+    "name:Health Professional InScope Protocol Content QC Report"
 ]
 
 #----------------------------------------------------------------------
