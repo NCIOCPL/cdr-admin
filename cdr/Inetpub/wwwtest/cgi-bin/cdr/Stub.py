8a06b3af464be035e29760137486ce6e6f7301cf
Bob Kline <bkline@rksystems.com>
2002-05-25 02:39:47 +0000

    <R1869> Script for reports that haven't been implemented yet.

---
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/Stub.py
@@ -0,0 +1,47 @@
+#----------------------------------------------------------------------
+#
+# $Id: Stub.py,v 1.1 2002-05-25 02:39:47 bkline Exp $
+#
+# Stub for report which hasn't been implemented yet.
+#
+# $Log: not supported by cvs2svn $
+#----------------------------------------------------------------------
+import cgi, cdr, cdrcgi, re, string
+
+#----------------------------------------------------------------------
+# Set the form variables.
+#----------------------------------------------------------------------
+fields  = cgi.FieldStorage()
+session = cdrcgi.getSession(fields)
+action  = cdrcgi.getRequest(fields)
+title   = "CDR Administration"
+section = "Reports"
+SUBMENU = "Reports Menu"
+buttons = [SUBMENU, cdrcgi.MAINMENU, "Log Out"]
+header  = cdrcgi.header(title, title, section, "Reports.py", buttons)
+
+#----------------------------------------------------------------------
+# Handle navigation requests.
+#----------------------------------------------------------------------
+if action == cdrcgi.MAINMENU:
+    cdrcgi.navigateTo("Admin.py", session)
+elif action == SUBMENU:
+    cdrcgi.navigateTo("Reports.py", session)
+
+#----------------------------------------------------------------------
+# Handle request to log out.
+#----------------------------------------------------------------------
+if action == "Log Out": 
+    cdrcgi.logout(session)
+
+#----------------------------------------------------------------------
+# Display available report choices.
+#----------------------------------------------------------------------
+form = """\
+    <INPUT TYPE='hidden' NAME='%s' VALUE='%s'>
+    This report has not yet been implemented, either because
+    we don't yet have the specs, or because it's behind higher-priority
+    tasks in the development task queue.
+""" % (cdrcgi.SESSION, session)
+
+cdrcgi.sendPage(header + form + "</FORM></BODY></HTML>")
