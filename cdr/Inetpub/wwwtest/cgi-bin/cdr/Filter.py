b68f533c14b6649bed2d59f3266cb823111a1fb4
Bob Kline <bkline@rksystems.com>
2002-07-25 01:51:27 +0000

    <R2174> Added option for DTD validation.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/Filter.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/Filter.py
@@ -1,11 +1,14 @@
 #----------------------------------------------------------------------
 #
-# $Id: Filter.py,v 1.4 2002-07-15 20:18:03 bkline Exp $
+# $Id: Filter.py,v 1.5 2002-07-25 01:51:27 bkline Exp $
 #
 # Transform a CDR document using an XSL/T filter and send it back to 
 # the browser.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.4  2002/07/15 20:18:03  bkline
+# Added textType argument to cdrcgi.sendPage() call.
+#
 # Revision 1.3  2002/01/23 18:50:22  mruben
 # allow up to 8 filters
 #
@@ -17,7 +20,7 @@
 #
 #
 #----------------------------------------------------------------------
-import cgi, cdr, cdrcgi, re
+import cgi, cdr, cdrcgi, cdrpub, re, string
 
 #----------------------------------------------------------------------
 # Get the parameters from the request.
@@ -27,6 +30,7 @@ fields  = cgi.FieldStorage() or cdrcgi.bail("No Request Found", title)
 session = cdr.login('rmk', '***REDACTED***') or cdrcgi.bail("Not Authorized", title)
 docId   = fields.getvalue(cdrcgi.DOCID) or cdrcgi.bail("No Document", title)
 filtId0 = fields.getvalue(cdrcgi.FILTER) or cdrcgi.bail("No Filter", title)
+valFlag = fields.getvalue('validate') or 0
 filtId  = [filtId0,
            fields.getvalue(cdrcgi.FILTER + "1", ""),
            fields.getvalue(cdrcgi.FILTER + "2", ""),
@@ -42,6 +46,61 @@ filtId  = [filtId0,
 doc = cdr.filterDoc(session, filtId, docId = docId)
 if type(doc) == type(()):
   doc = doc[0]
+
+#----------------------------------------------------------------------
+# Add a table row for an error or warning.
+#----------------------------------------------------------------------
+def addRow(type, line):
+    line = line.split(":", 3)
+    html = """\
+   <tr>
+    <td valign='top'>%s</td>
+    <td valign='top'>%s</td>
+    <td valign='top' align='right'>%s</td>
+    <td valign='top' align='right'>%s</td>
+    <td valign='top'>%s</td>
+   </tr>
+""" % (type, line[0], line[1], line[2], line[3])
+    return html
+
+#----------------------------------------------------------------------
+# Validate the document if requested.
+#----------------------------------------------------------------------
+if valFlag:
+    digits = re.sub('[^\d]', '', docId)
+    idNum  = string.atoi(digits)
+    errObj = cdrpub.validateDoc(doc, idNum)
+    html = """\
+<html>
+ <head>
+  <title>Validation results for CDR%010d</title>
+ </head>
+ <body>
+ <h2>Validation results for CDR%010d</h2>
+""" % (idNum, idNum)
+    if not errObj.Warnings and not errObj.Errors:
+        html += """\
+ <h3>Document comes out clean: no errors, no warnings!</h3>
+"""
+    else:
+        html += """\
+  <table border='1' cellspacing='0' cellpadding='2'>
+   <tr>
+    <th>Type</th>
+    <th>Document</th>
+    <th>Line</th>
+    <th>Position</th>
+    <th>Message</th>
+   </tr>
+"""
+        for warning in errObj.Warnings:
+            html += addRow('Warning', warning)
+        for error in errObj.Errors:
+            html += addRow('Error', error)
+    cdrcgi.sendPage(cdrcgi.unicodeToLatin1(html + """\
+ </body>
+</html>
+"""))
 
 doc = cdrcgi.decode(doc)
 doc = re.sub("@@DOCID@@", docId, doc)
