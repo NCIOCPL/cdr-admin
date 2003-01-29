e91aa84a3adf3f17bd0d33cbb60d3f38324f86ed
Bob Kline <bkline@rksystems.com>
2003-01-29 18:47:04 +0000

    <R3245> CGI script for querying the Oracle PDQ database.

---
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/PdqQueries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#----------------------------------------------------------------------
+#
+# $Id: PdqQueries.py,v 1.1 2003-01-29 18:47:04 bkline Exp $
+#
+# Ad-hoc SQL query tool for PDQ database.
+#
+# $Log: not supported by cvs2svn $
+#----------------------------------------------------------------------
+
+import win32api, cdrcgi, os
+os.environ['PATH'] = '%s;%s' % (r'd:\usr\oracle\ora92\bin', os.environ['PATH'])
+win32api.LoadLibrary("d:/usr/oracle/ora92/bin/oci.dll")
+#cdrcgi.bail(os.environ['PATH'])
+import CgiQuery, DCOracle2
+
+class PdqQuery(CgiQuery.CgiQuery):
+    def __init__(self):
+        conn = DCOracle2.Connect('bkline/bkline@pdq')
+        CgiQuery.CgiQuery.__init__(self, conn, "PDQ", "PdqQueries.py")
+
+pdqQuery = PdqQuery()
+pdqQuery.run()
