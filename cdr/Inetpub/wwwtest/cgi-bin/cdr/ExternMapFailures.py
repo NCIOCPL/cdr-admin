895e1a2cdbc668b08ac933603b3e5e1684332769
Bob Kline <bkline@rksystems.com>
2003-11-25 12:46:52 +0000

    <R4465> Minor formatting and encoding fixes.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/ExternMapFailures.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/ExternMapFailures.py
@@ -1,11 +1,15 @@
 #----------------------------------------------------------------------
 #
-# $Id: ExternMapFailures.py,v 1.1 2003-11-10 18:01:15 bkline Exp $
+# $Id: ExternMapFailures.py,v 1.2 2003-11-25 12:46:52 bkline Exp $
 #
 # Report on values found in external systems (such as ClinicalTrials.gov)
 # which have not yet been mapped to CDR documents.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2003/11/10 18:01:15  bkline
+# Report on values found in external systems (such as ClinicalTrials.gov)
+# which have not yet been mapped to CDR documents.
+#
 #----------------------------------------------------------------------
 import cdrdb, cdrcgi
 
@@ -49,11 +53,11 @@ html = """\
 while row:
     html += """\
    <tr>
+    <td nowrap = '1' valign='top'>%s</td>
     <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
+    <td nowrap = '1' valign='top'>%s</td>
    </tr>
-""" % (row[0], row[1].encode('utf-8'), row[2])
+""" % (row[0], cdrcgi.unicodeToLatin1(row[1]), row[2])
     row = cursor.fetchone()
 cdrcgi.sendPage(html + """\
   </table>
