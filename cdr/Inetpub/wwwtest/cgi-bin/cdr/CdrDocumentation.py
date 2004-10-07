7074a59af7aa5b9584f5bfeb85c75d9950cdfcb2
Volker Englisch <venglisch@users.noreply.github.com>
2004-10-07 19:34:37 +0000

    <R5520> Fixed the problem of the navigational buttons not working properly. (Bug 1338)

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/CdrDocumentation.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/CdrDocumentation.py
@@ -1,10 +1,14 @@
 #----------------------------------------------------------------------
 #
-# $Id: CdrDocumentation.py,v 1.1 2004-09-20 20:30:21 venglisc Exp $
+# $Id: CdrDocumentation.py,v 1.2 2004-10-07 19:34:37 venglisc Exp $
 #
 # Prototype for CDR reporting/formatting web wrapper.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2004/09/20 20:30:21  venglisc
+# Initial version of script to create statis page with links to PDF formatted
+# CDR documentation. (Bug 1338)
+#
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, string
 
@@ -12,8 +16,14 @@ import cgi, cdr, cdrcgi, re, string
 # Set the form variables.
 #----------------------------------------------------------------------
 fields  = cgi.FieldStorage()
+session = cdrcgi.getSession(fields)
 request = cdrcgi.getRequest(fields)
 SUBMENU = "Report Menu"
+
+#----------------------------------------------------------------------
+# Make sure we're logged in.
+#----------------------------------------------------------------------
+if not session: cdrcgi.bail('Unknown or expired CDR session.')
 
 #----------------------------------------------------------------------
 # Handle navigation requests.
@@ -21,14 +31,14 @@ SUBMENU = "Report Menu"
 if request == cdrcgi.MAINMENU:
     cdrcgi.navigateTo("Admin.py", session)
 elif request == SUBMENU:
-    cdrcgi.navigateTo("reports.py", session)
+    cdrcgi.navigateTo("Reports.py", session)
 
 #----------------------------------------------------------------------
 # Put out the form if we don't have a request.
 #----------------------------------------------------------------------
 title   = "CDR Administration"
 instr   = "CDR Documentation (PDF)"
-buttons = ("Submit Request", SUBMENU, cdrcgi.MAINMENU)
+buttons = (SUBMENU, cdrcgi.MAINMENU)
 header  = cdrcgi.header(title, title, instr, "CdrDocumentation.py", buttons)
 server  = "bach.nci.nih.gov"
 location= "cdr/Documentation"
@@ -37,9 +47,10 @@ usrdocs = [['CdrUserGuide.pdf',  'User Guide'],
 	   ['CdrOpsManual.pdf',  'Operation Manual']]
 
 form    = """\
+  <INPUT TYPE='hidden' NAME='%s' VALUE='%s'>
   <h3>CDR Documentation (PDF) as of 2004-09-08</h3>
   <ol>
-"""
+""" % (cdrcgi.SESSION, session)
 
 for row in usrdocs:
    form += """\
