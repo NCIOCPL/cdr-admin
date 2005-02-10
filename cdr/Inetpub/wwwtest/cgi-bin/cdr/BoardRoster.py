3dcb1b582b2f2e53f039cb2f7a2b97b290795f63
Bob Kline <bkline@rksystems.com>
2005-02-10 19:17:36 +0000

    <R5806> Converted from POST to GET form request; tightened up SQL query.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/BoardRoster.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/BoardRoster.py
@@ -1,21 +1,24 @@
 #----------------------------------------------------------------------
-# $Id: BoardRoster.py,v 1.1 2004-05-21 20:59:37 venglisc Exp $
+#
+# $Id: BoardRoster.py,v 1.2 2005-02-10 19:17:36 bkline Exp $
 #
 # Report to display the Board Roster with or without assistant
 # information.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2004/05/21 20:59:37  venglisc
+# Initial Version to create the PDQ Board Member Roster report.
+#
 #----------------------------------------------------------------------
-import cgi, cdr, cdrcgi, re, string, cdrdb, time
+import cgi, cdr, cdrcgi, cdrdb, time
 
 #----------------------------------------------------------------------
 # Set the form variables.
 #----------------------------------------------------------------------
 fields    = cgi.FieldStorage()
-boardInfo = fields and fields.getvalue("BoardInfo")      or None
-repType   = fields and fields.getvalue("RepType")        or None
-otherInfo = fields and fields.getvalue("ShowOtherInfo")     or 'No'
-assistant = fields and fields.getvalue("ShowAssistantInfo") or 'No'
+boardId   = fields and fields.getvalue("board") or None
+otherInfo = fields and fields.getvalue("oinfo") or 'No'
+assistant = fields and fields.getvalue("ainfo") or 'No'
 session   = cdrcgi.getSession(fields)
 request   = cdrcgi.getRequest(fields)
 title     = "PDQ Board Roster Report"
@@ -23,12 +26,8 @@ instr     = "Report on PDQ Board Roster"
 script    = "BoardRoster.py"
 SUBMENU   = "Report Menu"
 buttons   = (SUBMENU, cdrcgi.MAINMENU)
-stPath    = '/Summary/SummaryTitle'
-sbPath    = '/Summary/SummaryMetaData/PDQBoard/Board/@cdr:ref'
-sbmPath   = '/Summary/SummaryMetaData/PDQBoard/BoardMember/@cdr:ref'
-trimPat   = re.compile("[\s;]+$")
-bmPath    = '/Person/ProfessionalInformation/PDQBoardMembershipDetails/PDQ'\
-            '%Board/@cdr:ref'
+header    = cdrcgi.header(title, title, instr, script, buttons)
+boardId   = boardId and int(boardId) or None
 
 #----------------------------------------------------------------------
 # Handle navigation requests.
@@ -48,10 +47,26 @@ except cdrdb.Error, info:
     cdrcgi.bail('Database connection failure: %s' % info[1][0])
 
 #----------------------------------------------------------------------
-# Returns a copy of a doc title without trailing whitespace or semicolons.
+# Look up title of a board, given its ID.
 #----------------------------------------------------------------------
-def trim(s):
-    return trimPat.sub("", s)
+def getBoardName(id):
+    try:
+        cursor.execute("SELECT title FROM document WHERE id = ?", id)
+        rows = cursor.fetchall()
+        if not rows:
+            cdrcgi.bail('Failure looking up title for CDR%s' % id)
+        return cleanTitle(rows[0][0])
+    except Exception, e:
+        cdrcgi.bail('Looking up board title: %s' % str(e))
+
+#----------------------------------------------------------------------
+# Remove cruft from a document title.
+#----------------------------------------------------------------------
+def cleanTitle(title):
+    semicolon = title.find(';')
+    if semicolon != -1:
+        title = title[:semicolon]
+    return title.strip()
 
 #----------------------------------------------------------------------
 # Build a picklist for PDQ Boards.
@@ -60,9 +75,9 @@ def trim(s):
 # b)  create a dictionary in subsequent calls to select the board
 #     ID based on the board selected in the first call.
 #----------------------------------------------------------------------
-def getBoardPicklist(boardDict):
-    picklist = "<SELECT NAME='BoardInfo'>"
-    selected = " SELECTED"
+def getBoardPicklist():
+    picklist = "<SELECT NAME='board'>\n"
+    sel      = " SELECTED"
     try:
         cursor.execute("""\
 SELECT DISTINCT board.id, board.title
@@ -73,26 +88,19 @@ SELECT DISTINCT board.id, board.title
             AND org_type.value IN ('PDQ Editorial Board',
                                    'PDQ Advisory Board')
        ORDER BY board.title""")
-        for row in cursor.fetchall():
-            semi = row[1].find(';')
-            if semi != -1: boardTitle = trim(row[1][:semi])
-            else:          boardTitle = trim(row[1])
-            picklist += "<OPTION%s>%s</OPTION>" % (selected, boardTitle)
-            selected = ""
-            boardDict[boardTitle] = row[0]
+        for id, title in cursor.fetchall():
+            title = cleanTitle(title)
+            picklist += "<OPTION%s value='%d'>%s</OPTION>\n" % (sel, id, title)
+            sel = ""
     except cdrdb.Error, info:
         cdrcgi.bail('Database query failure: %s' % info[1][0])
-    return picklist + "</SELECT>"
+    return picklist + "</SELECT>\n"
 
 #----------------------------------------------------------------------
 # If we don't have a request, put up the form.
 #----------------------------------------------------------------------
-boardList = {}
-if not boardInfo:
-    header   = cdrcgi.header(title, title, instr, script, ("Submit",
-                                                           SUBMENU,
-                                                           cdrcgi.MAINMENU))
-    form     = """\
+if not boardId:
+    form   = """\
       <INPUT TYPE='hidden' NAME='%s' VALUE='%s'>
       <TABLE>
        <TR>
@@ -102,84 +110,64 @@ if not boardInfo:
        <TR>
         <TD ALIGN='right'> </TD>
         <TD>
-         <INPUT TYPE='checkbox' NAME='ShowOtherInfo' VALUE='Yes'>
+         <INPUT TYPE='checkbox' NAME='oinfo'>
          Show All Contact Information
         </TD>
        </TR>
        <TR>
         <TD ALIGN='right'> </TD>
         <TD>
-         <INPUT TYPE='checkbox' NAME='ShowAssistantInfo' VALUE='Yes'>
+         <INPUT TYPE='checkbox' NAME='ainfo'>
          Show Assistant Information
         </TD>
        </TR>
        </TABLE>
+       <SCRIPT language='JavaScript'>
+        <!--
+         function report() {
+             var form = document.forms[0];
+             if (!form.board.value) {
+                 alert('Select a board first!');
+             }
+             else {
+                 form.oinfo.value = form.oinfo.checked ? 'Yes' : 'No';
+                 form.ainfo.value = form.ainfo.checked ? 'Yes' : 'No';
+                 form.method      = 'GET';
+                 form.submit();
+             }
+         }
+        // -->
+       </SCRIPT>
+       <BR>
+       <INPUT type='button' onclick='javascript:report()' value='Report'>
       </FORM>
      </BODY>
     </HTML>
-""" % (cdrcgi.SESSION, session, getBoardPicklist(boardList))
-
+""" % (cdrcgi.SESSION, session, getBoardPicklist())
     cdrcgi.sendPage(cdrcgi.unicodeToLatin1(header + form))
 
 #----------------------------------------------------------------------
-# Build date string for header.
+# Get the board's name from its ID.
 #----------------------------------------------------------------------
-dateString = time.strftime("%B %d, %Y", time.localtime(time.time()))
+boardName = getBoardName(boardId)
 
 #----------------------------------------------------------------------
-# We have a board specified; extract its ID based on its doc title
-# as the key of a dictionary created with getBoardPicklist().
+# Object for one PDQ board member.
 #----------------------------------------------------------------------
-getBoardPicklist(boardList)
-boardId    = boardList[boardInfo]
-boardName  = boardInfo
-if not boardId: 
-    cdrcgi.bail("Board information garbled: %s" % boardInfo)
-
-report    = ''
-html      = ''
-report += """\
-  </TABLE>
- </BODY>
-</HTML>
-"""
-#    except cdrdb.Error, info:
-#        cdrcgi.bail('Database query failure: %s' % info[1][0])
-#    cdrcgi.sendPage(cdrcgi.unicodeToLatin1(header + report))
-header    = cdrcgi.header(title, title, instr, script, buttons)
-# cdrcgi.sendPage(cdrcgi.unicodeToLatin1(header + report))
-
-#----------------------------------------------------------------------
-# Show the members of the board, with associated topics.
-#----------------------------------------------------------------------
-instr     = '%s Roster -- %s.' % (boardName, dateString)
-header    = cdrcgi.header(title, title, instr, script, buttons)
-members   = {}
-topics    = {}
-audString = ""
-report    = """\
-   <INPUT TYPE='hidden' NAME='%s' VALUE='%s'>
-  </FORM>
-  <H4>%s Roster%s</H4>
-""" % (cdrcgi.SESSION, session, boardName, audString)
-
-#----------------------------------------------------------------------
-# Type for storing board member information.
-#----------------------------------------------------------------------
-class Member:
-    def __init__(self, id, name):
-        self.id     = id
-        self.name   = name
-        self.topics = []
-
-#----------------------------------------------------------------------
-# Type for storing summary topic information.
-#----------------------------------------------------------------------
-class Topic:
-    def __init__(self, id, name):
-        self.id   = id
-        self.name = name
-
+class BoardMember:
+    now = time.strftime("%Y-%m-%d")
+    def __init__(self, docId, start, finish, name):
+        self.id    = docId
+        self.name  = cleanTitle(name)
+        self.isEic = (start and start >= BoardMember.now and
+                      (not finish or finish <= BoardMember.now))
+    def __cmp__(self, other):
+        if self.isEic == other.isEic:
+            return cmp(self.name.upper(), other.name.upper())
+        elif self.isEic:
+            return -1
+        return 1
+    
 #----------------------------------------------------------------------
 # This is trickier than the other form of the report, because we must
 # deal with two possible conditions:
@@ -187,67 +175,43 @@ class Topic:
 #      summaries.
 #   2. A summary lists a board member whose own document does not
 #      reflect that membership.
+#
+# XXX Note: Comment not reflected in code.  RMK 2005-02-10
 #----------------------------------------------------------------------
 try:
-    query = """\
-SELECT bp.int_val person_id, title person_name, b.doc_id boardmemberdoc_id, 
-       b.int_val board_id, bn.value board_name, d.doc_type, d.val_status,
-       CASE WHEN eds.value < getdate() THEN 'Active'
-            ELSE 'Inactive'
-       END EICS,
-       CASE WHEN ede.value IS NULL     THEN 'Active'
-            WHEN ede.value > getdate() THEN 'Active'
-            ELSE 'Inactive'
-       END EICE
-  FROM query_term b
-  JOIN document d  
-    ON d.id = b.doc_id
-  JOIN query_term bp
-                              -- get the board member person ID
-    ON b.doc_id = bp.doc_id
-  JOIN query_term bn                 
-                              -- get the board name
-    ON bn.doc_id = b.int_val
-  JOIN query_term curr               
-                              -- get the info if board member is active
-    ON b.doc_id = curr.doc_id
-   AND LEFT(b.node_loc, 4) = LEFT(curr.node_loc, 4)
-LEFT OUTER JOIN query_term eds       
-                              -- Find TermStartDate to find out 
-                              -- if EditorInChief
-    ON b.doc_id = eds.doc_id
-   AND left(b.node_loc, 4) = left(eds.node_loc, 4)
-   AND eds.path   = 
-      '/PDQBoardMemberInfo/BoardMembershipDetails/EditorInChief/TermStartDate'
-LEFT OUTER JOIN query_term ede
-                              -- Find TermEndDate to find out if still 
-                              -- active EditorInChief
-    ON b.doc_id = ede.doc_id  
-   AND left(b.node_loc, 4) = left(ede.node_loc, 4)
-   AND ede.path   = 
-          '/PDQBoardMemberInfo/BoardMembershipDetails/EditorInChief/TermEndDate'
- WHERE b.path     = 
-                 '/PDQBoardMemberInfo/BoardMembershipDetails/BoardName/@cdr:ref'
-                              -- get the records with the board id
-   AND b.int_val  = %d
-                              -- value if the board id
-   AND bp.path    = '/PDQBoardMemberInfo/BoardMemberName/@cdr:ref'
-                              -- get the person ID from the board document
-   AND bn.path   = '/Organization/OrganizationNameInformation/OfficialName/Name'
-                              -- get the official board name
-   AND active_status = 'A' 
-                              -- document can not be blocked
-   AND curr.path  = '/PDQBoardMemberInfo/BoardMembershipDetails/CurrentMember'
-                              -- Board member must be current
-   AND curr.value = 'Yes'
- ORDER BY 8, title""" % (boardId) 
-    cursor.execute(query)
-    boardmember = []
-    for row in cursor.fetchall():
-        eic = ''
-        if row[7] == 'Active' and row[8] == 'Active':
-            eic = 'Yes'
-        boardmember.append([row[0], row[2], eic])
+    cursor.execute("""\
+ SELECT DISTINCT member.doc_id, start.value, finish.value, person_doc.title
+            FROM query_term member
+            JOIN query_term curmemb
+              ON curmemb.doc_id = member.doc_id
+            JOIN query_term person
+              ON person.doc_id = member.doc_id
+            JOIN document person_doc
+              ON person_doc.id = person.doc_id
+ LEFT OUTER JOIN query_term start
+              ON start.doc_id = member.doc_id
+             AND LEFT(start.node_loc, 4) = LEFT(member.node_loc, 4)
+             AND start.path   = '/PDQBoardMemberInfo/BoardMembershipDetails'
+                              + '/EditorInChief/TermStartDate'
+ LEFT OUTER JOIN query_term finish
+              ON finish.doc_id = member.doc_id  
+             AND LEFT(finish.node_loc, 4) = LEFT(member.node_loc, 4)
+             AND finish.path  = '/PDQBoardMemberInfo/BoardMembershipDetails'
+                              + '/EditorInChief/TermEndDate'
+           WHERE member.path  = '/PDQBoardMemberInfo/BoardMembershipDetails'
+                              + '/BoardName/@cdr:ref'
+             AND curmemb.path = '/PDQBoardMemberInfo/BoardMembershipDetails'
+                              + '/CurrentMember'
+             AND person.path  = '/PDQBoardMemberInfo/BoardMemberName/@cdr:ref'
+             AND curmemb.value = 'Yes'
+             AND person_doc.active_status = 'A'
+             AND member.int_val = ?""", boardId, timeout = 300)
+    rows = cursor.fetchall()
+    boardMembers = []
+    for docId, start, finish, name in rows:
+        boardMembers.append(BoardMember(docId, start, finish, name))
+    boardMembers.sort()
+
 except cdrdb.Error, info:
     cdrcgi.bail('Database query failure: %s' % info[1][0])
 
@@ -275,16 +239,20 @@ html = """\
   <CENTER>
    <H1>%s</H1>
   </CENTER>
-""" % (boardInfo, boardInfo)   
+""" % (boardName, boardName)   
 
-for i in boardmember:
-    response = cdr.filterDoc('guest', ['set:Denormalization PDQBoardMemberInfo Set',
-                                       'name:Copy XML for Person 2',
-                                       'name:PDQBoardMember Roster'], i[1],
-                                       parm = [['otherInfo',otherInfo],
-                                               ['assistant', assistant],
-                                               ['eic', i[2]]])
-                                               #['eic', i[2] ]) 
+for boardMember in boardMembers:
+    response = cdr.filterDoc('guest',
+                             ['set:Denormalization PDQBoardMemberInfo Set',
+                              'name:Copy XML for Person 2',
+                              'name:PDQBoardMember Roster'],
+                             boardMember.id,
+                             parm = [['otherInfo', otherInfo],
+                                     ['assistant', assistant],
+                                     ['eic',
+                                      boardMember.isEic and 'Yes' or 'No']])
+    if type(response) in (str, unicode):
+        cdrcgi.bail("%s: %s" % (boardMember.id, response))
     html += response[0]
 
 html += """
