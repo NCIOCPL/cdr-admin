c2fbfd0b967ccab637cbb2977141e1cc74dee99f
Bob Kline <bkline@rksystems.com>
2005-03-14 16:31:27 +0000

    <R5914> Check to verify that the Python CGI mechanism is alive.

---
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/ping.py
@@ -0,0 +1,11 @@
+print """\
+Content-type: text/html
+
+<!DOCTYPE HTML PUBLIC '-//IETF//DTD HTML//EN'>
+<html>
+ <head>
+  <title>Alive</title>
+ </head>
+ <body>Python mapping is alive . . .</body>
+</html>
+"""
