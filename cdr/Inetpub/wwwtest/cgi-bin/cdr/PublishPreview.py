f9dca4f5550f0a9706055ff5479a54e4f39e8e0f
Volker Englisch <venglisch@users.noreply.github.com>
2005-12-08 16:01:32 +0000

    <R6439> Modified the program to handle an additional parameter.  With this new version parameter we are able to display a specific version of a document including the cwd (current working document).

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/PublishPreview.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/PublishPreview.py
@@ -1,11 +1,14 @@
 #----------------------------------------------------------------------
 #
-# $Id: PublishPreview.py,v 1.26 2005-04-21 21:30:06 venglisc Exp $
+# $Id: PublishPreview.py,v 1.27 2005-12-08 16:01:32 venglisc Exp $
 #
 # Transform a CDR document using an XSL/T filter and send it back to 
 # the browser.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.26  2005/04/21 21:30:06  venglisc
+# Modified to allow running GlossaryTerm Publish Preview reports. (Bug 1531)
+#
 # Revision 1.25  2005/02/15 13:07:13  bkline
 # Added ability to override default for host from which to pull the
 # stylesheet.
@@ -93,22 +96,24 @@ title   = "CDR Publish Preview"
 fields  = cgi.FieldStorage() or None
 
 if not fields:
-    session = 'guest'
-    docId   = sys.argv[1]
-    dbgLog  = len(sys.argv) > 2 and sys.argv[2] or None
-    flavor  = len(sys.argv) > 3 and sys.argv[3] or None
-    cgHost  = len(sys.argv) > 4 and sys.argv[4] or None
-    ssHost  = len(sys.argv) > 5 and sys.argv[5] or 'www.cancer.gov'
-    monitor = 1
+    session    = 'guest'
+    docId      = sys.argv[1]
+    docVersion = len(sys.argv) > 2 and sys.argv[2] or None
+    dbgLog     = len(sys.argv) > 3 and sys.argv[3] or None
+    flavor     = len(sys.argv) > 4 and sys.argv[4] or None
+    cgHost     = len(sys.argv) > 5 and sys.argv[5] or None
+    ssHost     = len(sys.argv) > 6 and sys.argv[6] or 'www.cancer.gov'
+    monitor    = 1
 else:
-    session = cdrcgi.getSession(fields) or cdrcgi.bail("Not logged in")
-    docId   = fields.getvalue(cdrcgi.DOCID) or cdrcgi.bail("No Document",
+    session    = cdrcgi.getSession(fields)   or cdrcgi.bail("Not logged in")
+    docId      = fields.getvalue(cdrcgi.DOCID) or cdrcgi.bail("No Document",
                                                            title)
-    flavor  = fields.getvalue("Flavor") or None
-    dbgLog  = fields.getvalue("DebugLog") or None
-    cgHost  = fields.getvalue("cgHost") or None
-    ssHost  = fields.getvalue("ssHost") or 'www.cancer.gov'
-    monitor = 0
+    docVersion = fields.getvalue("Version")  or None
+    flavor     = fields.getvalue("Flavor")   or None
+    dbgLog     = fields.getvalue("DebugLog") or None
+    cgHost     = fields.getvalue("cgHost")   or None
+    ssHost     = fields.getvalue("ssHost")   or 'www.cancer.gov'
+    monitor    = 0
 
 #----------------------------------------------------------------------
 # Point to a different host if requested.
@@ -148,29 +153,68 @@ except cdrdb.Error, info:
 #----------------------------------------------------------------------
 # Determine the document type and version.
 #----------------------------------------------------------------------
+# We are covering three situations here.
+# a) No Version parameter is given
+#    The original behavior will be performed, namely to display the 
+#    latest publishable version of the document if it exists.
+# b) The Version parameter is set to 'cwd'
+#    The current working document will be displayed
+# c) The Version parameter is set and it is not 'cwd'
+#    This means that the specified version is to be displayed in the
+#    publish preview display.
+# -------------------------------------------------------------------
 try:
     digits = re.sub('[^\d]+', '', docId)
     intId  = int(digits)
+
+    # We need to select the document type
+    # -----------------------------------
     cursor.execute("""\
-        SELECT doc_type.name, MAX(doc_version.num)
+        SELECT doc_type.name
           FROM doc_type
           JOIN document
             ON document.doc_type = doc_type.id
-          JOIN doc_version
-            ON doc_version.id = document.id
-         WHERE document.id = ?
-           AND doc_version.publishable = 'Y'
-      GROUP BY doc_type.name""", (intId,))
-
+         WHERE document.id = ?""", (intId,))
+    
     row = cursor.fetchone()
     if not row:
-        cdrcgi.bail("Unable to find publishable version for %s" % docId)
-    docType, docVer = row
+        cdrcgi.bail("Unable to find specified document %s" % docId)
+
+    docType = row[0]
+
+    # Next we need to identify the version to be displayed
+    # ----------------------------------------------------
+    if not docVersion:
+        cursor.execute("""\
+        SELECT max(doc_version.num)
+          FROM doc_version
+         WHERE doc_version.id = ?
+           AND doc_version.publishable = 'Y' """, (intId,))
+
+        row = cursor.fetchone()
+        if not row:
+            cdrcgi.bail("Unable to find publishable version for %s" % docId)
+        docVer = row[0]
+    elif docVersion != 'cwd':
+        cursor.execute("""\
+        SELECT doc_version.num
+          FROM doc_version
+         WHERE doc_version.id = ?
+           AND doc_version.num = ? """, (intId, docVersion,))
+
+        row = cursor.fetchone()
+        if not row:
+            cdrcgi.bail("Unable to find specified version %s for %s" % (
+                                                            docVersion, docId))
+        docVer = row[0]
+    else:
+        docVer = None
+
 except cdrdb.Error, info:    
-        cdrcgi.bail('Failure finding publishable version for %s: %s' % (docId, 
+        cdrcgi.bail('Failure finding specified version for %s: %s' % (docId, 
                                                                  info[1][0]))
 showProgress("Fetched document type: %s..." % row[0])
-showProgress("Fetched latest version number: %d..." % row[1])
+
 if not flavor:
     if docType == "Summary":           flavor = "summary"
     elif docType == "InScopeProtocol": flavor = "protocol_hp"
