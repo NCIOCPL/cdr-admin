e9d2bebe7c0854357ac4916dceb84d411ad5e0b0
Bob Kline <bkline@rksystems.com>
2006-05-08 18:18:40 +0000

    <R6630> Added Media Tracking Report, and some rewording (at Margaret's request); see request 2135.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/MediaReports.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/MediaReports.py
@@ -1,8 +1,11 @@
 #----------------------------------------------------------------------
 #
-# $Id: MediaReports.py,v 1.2 2006-05-04 14:04:56 bkline Exp $
+# $Id: MediaReports.py,v 1.3 2006-05-08 18:18:40 bkline Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2006/05/04 14:04:56  bkline
+# Added MediaSearcy.py.
+#
 # Revision 1.1  2005/05/04 18:14:01  venglisc
 # Inintial version of Media Reports Menu page of the Admin interface.
 # (Bug 1653)
@@ -44,12 +47,18 @@ session = "%s=%s" % (cdrcgi.SESSION, session)
 form = """\
     <H3>QC Reports</H3>
     <OL>
-    <LI><a href='MediaSearch.py?%s'>Advanced Media Search</a></LI>
+    <LI><a href='MediaSearch.py?%s'>Advanced Search</a></LI>
 """ % session
 for choice in (
-    ('img',  'Media Reports'   ),
+    ('img',  'Media Doc QC Report'   ),
     ):
     form += """\
     <LI><a href='%s/QcReport.py?DocType=Media&ReportType=%s&%s'>%s</a></LI>
 """ % (cdrcgi.BASE, choice[0], session, choice[1])
+form += "</OL>\n<H3>Management Reports</H3><OL>\n"
+for choice in(
+    ('MediaTrackingReport.py', 'Media Tracking Report'),
+    ):
+    form += "<LI><a href='%s/%s?%s'>%s</a></LI>\n" % (cdrcgi.BASE, choice[0],
+                                                      session, choice[1])
 cdrcgi.sendPage(header + form + "</OL></FORM></BODY></HTML>")
