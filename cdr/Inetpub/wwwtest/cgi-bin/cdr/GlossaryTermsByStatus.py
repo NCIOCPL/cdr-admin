ecefd910e8386058253445205a66e8c5ff9116dd
Bob Kline <bkline@rksystems.com>
2006-05-16 18:54:31 +0000

    <R6654> Stripped out Spanish version, which needs to be run in batch mode now to accomodate new requirements from Sheri.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/GlossaryTermsByStatus.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/GlossaryTermsByStatus.py
@@ -1,12 +1,15 @@
 #----------------------------------------------------------------------
 #
-# $Id: GlossaryTermsByStatus.py,v 1.3 2006-05-04 13:59:06 bkline Exp $
+# $Id: GlossaryTermsByStatus.py,v 1.4 2006-05-16 18:54:31 bkline Exp $
 #
 # The Glossary Terms by Status Report will server as a QC report to check
 # which glossary terms were created within a given time frame with a
 # particular status set.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2006/05/04 13:59:06  bkline
+# More (extensive) changes for Sheri (same issue: 1861).
+#
 # Revision 1.2  2005/08/18 15:00:04  bkline
 # Modifications request by Sheri (#1790).
 #
@@ -26,7 +29,6 @@ session  = fields and fields.getvalue("Session") or None
 request  = cdrcgi.getRequest(fields)
 fromDate = fields and fields.getvalue('FromDate') or None
 toDate   = fields and fields.getvalue('ToDate') or None
-lang     = fields and fields.getvalue('lang') or None
 title    = "CDR Administration"
 instr    = "Glossary Terms by Status"
 buttons  = ["Submit Request", "Report Menu", cdrcgi.MAINMENU, "Log Out"]
@@ -43,18 +45,11 @@ elif request == "Report Menu":
 elif request == "Log Out": 
     cdrcgi.logout(session)
 
-def getOptions(lang):
-    if not lang:
-        return """\
+def getOptions():
+    return """\
        <OPTION VALUE='Approved'>Approved</OPTION>
        <OPTION VALUE='Pending'>Pending</OPTION>
        <OPTION VALUE='Revision pending'>Revision pending</OPTION>"""
-    else:
-        return ("""\
-       <OPTION VALUE='Translation approved'>Translation approved</OPTION>
-       <OPTION VALUE='Translation pending'>Translation pending</OPTION>
-       <OPTION VALUE='Translation revision pending'>"""
-                """Translation revision pending</OPTION>""")
 
 #----------------------------------------------------------------------
 # As the user for the report parameters.
@@ -93,11 +88,10 @@ if not fromDate or not toDate or not status:
      </TD>
     </TR>
    </TABLE>
-   <INPUT TYPE='hidden' NAME='lang' VALUE='%s'>
   </FORM>
  </BODY>
 </HTML>
-""" % (cdrcgi.SESSION, session, fromDate, toDate, getOptions(lang), lang or "")
+""" % (cdrcgi.SESSION, session, fromDate, toDate, getOptions())
     cdrcgi.sendPage(header + form)
 
 #----------------------------------------------------------------------
@@ -154,20 +148,7 @@ revisionInfo = status.upper() == 'REVISION PENDING'
 conn = cdrdb.connect('CdrGuest')
 cursor = conn.cursor()
 
-if lang:
-    cursor.execute("""\
-    SELECT DISTINCT d.doc_id, d.value
-               FROM query_term d
-               JOIN query_term s
-                 ON d.doc_id = s.doc_id
-                AND LEFT(d.node_loc, 4) = LEFT(s.node_loc, 4)
-              WHERE d.path = '/GlossaryTerm/SpanishTermDefinition/StatusDate'
-                AND s.path = '/GlossaryTerm/SpanishTermDefinition'
-                           + '/DefinitionStatus'
-                AND s.value = ?
-                AND d.value BETWEEN '%s' AND '%s'""" % (fromDate, toDate),
-                   status, timeout = 300)
-elif revisionInfo:
+if revisionInfo:
     cursor.execute("""\
     SELECT DISTINCT s.doc_id, MAX(v.dt)
       FROM query_term s
@@ -191,7 +172,7 @@ else:
                (fromDate, toDate), status, timeout = 300)
 
 class GlossaryTerm:
-    def __init__(self, id, node, pubNode = None, spDefStatusDate = None):
+    def __init__(self, id, node, pubNode = None):
         self.id = id
         self.name = None
         self.pronunciation = None
@@ -199,11 +180,8 @@ class GlossaryTerm:
         self.source = None
         self.pubPronunciation = None
         self.pubDefinitions = []
-        self.spDefinitions = []
         self.types = []
         self.statusDate = None
-        self.spDefStatusDate = spDefStatusDate
-        self.spTermName = None
         self.translationResource = None
         for child in node.childNodes:
             if child.nodeName == "TermName":
@@ -221,17 +199,6 @@ class GlossaryTerm:
                 self.types.append(getNodeContent(child))
             elif child.nodeName == "StatusDate":
                 self.statusDate = getNodeContent(child)
-            elif spDefStatusDate:
-                if child.nodeName == 'SpanishTermName':
-                    self.spTermName = getNodeContent(child)
-                if child.nodeName == 'SpanishTermDefinition':
-                    for grandchild in child.childNodes:
-                        if grandchild.nodeName == 'DefinitionText':
-                            defText = getNodeContent(grandchild)
-                            self.spDefinitions.append(defText)
-                        elif grandchild.nodeName == 'TranslationResource':
-                            trRes = getNodeContent(grandchild)
-                            self.translationResource = trRes
         if pubNode:
             for child in pubNode.childNodes:
                 if child.nodeName == "TermPronunciation":
@@ -255,8 +222,7 @@ for row in cursor.fetchall():
                              version = str(versions[1]))
             pdom = xml.dom.minidom.parseString(doc.xml)
     terms.append(GlossaryTerm(row[0], dom.documentElement,
-                              pdom and pdom.documentElement or None,
-                              lang and row[1] or None))
+                              pdom and pdom.documentElement or None))
 terms.sort(lambda a,b: cmp(a.name, b.name))
 html = u"""\
 <!DOCTYPE HTML PUBLIC '-//IETF//DTD HTML//EN'>
@@ -282,32 +248,7 @@ html = u"""\
   </center>
   <table border='1' cellspacing='0' cellpadding='2' width='100%%'>
 """ % (status, fromDate, toDate)
-if lang:
-    if status.upper() == 'TRANSLATION REVISION PENDING':
-        html += """\
-   <tr>
-    <th>CDR ID</th>
-    <th>English Term</th>
-    <th>Spanish Term</th>
-    <th>Approved English Definition Revision</th>
-    <th>Pending Spanish Translation Revision</th>
-    <th>Translation Resource</th>
-    <th>Status Date</th>
-   </tr>
-"""
-    else:
-        html += """\
-   <tr>
-    <th>CDR ID</th>
-    <th>English Term</th>
-    <th>Spanish Term</th>
-    <th>English Definition</th>
-    <th>Spanish Translation</th>
-    <th>Translation Resource</th>
-    <th>Status Date</th>
-   </tr>
-"""
-elif revisionInfo:
+if revisionInfo:
     html += """\
    <tr>
     <th>CDR ID</th>
@@ -334,25 +275,7 @@ else:
    </tr>
 """
 for term in terms:
-    if lang:
-        html += u"""\
-   <tr>
-    <td>%d</td>
-    <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
-    <td>%s</td>
-   </tr>
-""" % (term.id,
-       fix(term.name),
-       fix(term.spTermName),
-       fixList(term.definitions),
-       fixList(term.spDefinitions),
-       fix(term.translationResource),
-       term.spDefStatusDate and term.spDefStatusDate[:10] or u"&nbsp;")
-    elif revisionInfo:
+    if revisionInfo:
         html += u"""\
    <tr>
     <td>%d</td>
