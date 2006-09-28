588dd89b01bd26f6412d9c4338a62c2af4e4d5cb
ameyer <webcommauto@users.noreply.github.com>
2006-09-28 23:09:43 +0000

    <R6978> Added links for HTML versions of current docs. Updated date on PDF.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/CdrDocumentation.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/CdrDocumentation.py
@@ -1,10 +1,14 @@
 #----------------------------------------------------------------------
 #
-# $Id: CdrDocumentation.py,v 1.2 2004-10-07 19:34:37 venglisc Exp $
+# $Id: CdrDocumentation.py,v 1.3 2006-09-28 23:09:43 ameyer Exp $
 #
 # Prototype for CDR reporting/formatting web wrapper.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2004/10/07 19:34:37  venglisc
+# Fixed the problem of the navigational buttons not working properly.
+# (Bug 1338)
+#
 # Revision 1.1  2004/09/20 20:30:21  venglisc
 # Initial version of script to create statis page with links to PDF formatted
 # CDR documentation. (Bug 1338)
@@ -42,13 +46,18 @@ buttons = (SUBMENU, cdrcgi.MAINMENU)
 header  = cdrcgi.header(title, title, instr, "CdrDocumentation.py", buttons)
 server  = "bach.nci.nih.gov"
 location= "cdr/Documentation"
+cgiLoc  = "cgi-bin/cdr"
 usrdocs = [['CdrUserGuide.pdf',  'User Guide'],
-           ['CdrSystemDocs.pdf', 'System Documentation'], 
-	   ['CdrOpsManual.pdf',  'Operation Manual']]
+           ['CdrSystemDocs.pdf', 'System Documentation'],
+	       ['CdrOpsManual.pdf',  'Operation Manual']]
+usrhtml = [['Help.py', 'User Guild'],
+           ['Help.py?flavor=System%', 'System Documentation'],
+           ['Help.py?flavor=Operating%Instructions', 'Operation Manual']]
+
 
 form    = """\
   <INPUT TYPE='hidden' NAME='%s' VALUE='%s'>
-  <h3>CDR Documentation (PDF) as of 2004-09-08</h3>
+  <h3>CDR Documentation (PDF) as of 2006-09-28</h3>
   <ol>
 """ % (cdrcgi.SESSION, session)
 
@@ -56,6 +65,16 @@ for row in usrdocs:
    form += """\
    <li><a href="http://%s/%s/%s">%s</a></li>
 """ % (server, location, row[0], row[1])
+
+form += """\
+  </ol>
+  <h3>CDR Documentation (HTML) - Current</h3>
+  <ol>
+"""
+for row in usrhtml:
+   form += """\
+   <li><a href="http://%s/%s/%s">%s</a></li>
+""" % (server, cgiLoc, row[0], row[1])
 
 form    += """\
   </ol>
