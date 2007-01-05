a6becfc262c956c1aad923e0ba0a707abe017c08
Volker Englisch <venglisch@users.noreply.github.com>
2007-01-05 23:18:40 +0000

    <R7140> New publishing reports menu file. (Bug 2111)

---
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/PublishReports.py
@@ -0,0 +1,54 @@
+#----------------------------------------------------------------------
+#
+# $Id: PublishReports.py,v 1.1 2007-01-05 23:18:40 venglisc Exp $
+#
+# Submenu for publishing reports.
+#
+# $Log: not supported by cvs2svn $
+#----------------------------------------------------------------------
+import cgi, cdr, cdrcgi, re, string
+
+#----------------------------------------------------------------------
+# Set the form variables.
+#----------------------------------------------------------------------
+fields  = cgi.FieldStorage()
+session = cdrcgi.getSession(fields)
+action  = cdrcgi.getRequest(fields)
+title   = "CDR Administration"
+section = "Publishing Reports"
+SUBMENU = "Reports Menu"
+buttons = [SUBMENU, cdrcgi.MAINMENU, "Log Out"]
+header  = cdrcgi.header(title, title, section, "Reports.py", buttons)
+
+#----------------------------------------------------------------------
+# Handle navigation requests.
+#----------------------------------------------------------------------
+if action == cdrcgi.MAINMENU:
+    cdrcgi.navigateTo("Admin.py", session)
+elif action == SUBMENU:
+    cdrcgi.navigateTo("Reports.py", session)
+
+#----------------------------------------------------------------------
+# Handle request to log out.
+#----------------------------------------------------------------------
+if action == "Log Out": 
+    cdrcgi.logout(session)
+
+#----------------------------------------------------------------------
+# Display available report choices.
+#----------------------------------------------------------------------
+form = """\
+    <INPUT TYPE='hidden' NAME='%s' VALUE='%s'>
+    <OL>
+""" % (cdrcgi.SESSION, session)
+reports = [
+           ('PubStatus.py',
+            'Publishing Job Activities'),
+           ('PubStatsByDate.py',
+            'Publishing Job Statistics')
+          ]
+for r in reports:
+    form += "<LI><A HREF='%s/%s?%s=%s'>%s</LI></A>\n" % (
+            cdrcgi.BASE, r[0], cdrcgi.SESSION, session, r[1])
+
+cdrcgi.sendPage(header + form + "</OL></FORM></BODY></HTML>")
