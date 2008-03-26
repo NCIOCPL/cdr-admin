728665fdd11c84537b243d725c2577b9bd7c2e38
Volker Englisch <venglisch@users.noreply.github.com>
2008-03-26 20:41:23 +0000

    <R8066> Modified program to allow a document ID passed as CDR9999-111.jpg where the number after the dash is indicating the size, to be processed. This is the format we are receiving from Cancer.gov as part of the publish preview HTML output. (Bug 3973)

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/GetCdrImage.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/GetCdrImage.py
@@ -1,10 +1,13 @@
 #----------------------------------------------------------------------
 #
-# $Id: GetCdrImage.py,v 1.1 2004-11-17 14:39:55 bkline Exp $
+# $Id: GetCdrImage.py,v 1.2 2008-03-26 20:41:23 venglisc Exp $
 #
 # Send JPEG version of a CDR image to the browser, possibly resized.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2004/11/17 14:39:55  bkline
+# New scripts to support images in the CDR.
+#
 #----------------------------------------------------------------------
 import cdrdb, sys, cgi, msvcrt, os, cdr, cdrcgi, Image, cStringIO
 
@@ -14,9 +17,20 @@ fields  = cgi.FieldStorage()
 width   = fields and fields.getvalue("width")   or None
 fname   = fields and fields.getvalue("fname")   or None
 quality = fields and fields.getvalue("quality") or "75"
-docId   = fields and fields.getvalue("id")      or cdrcgi.bail(message)
+cdrId   = fields and fields.getvalue("id")      or cdrcgi.bail(message)
+
 conn    = cdrdb.connect()
 cursor  = conn.cursor()
+
+# If the docId comes in in the format 'CDR99999-111.jpg' it is coming 
+# from the PublishPreview with a size postfix.
+# --------------------------------------------------------------------
+if cdrId.find('-') > 0:
+    docId = cdrId.split('-')[0]
+    width = cdrId.split('-')[1].split('.')[0]
+else:
+    docId = cdrId
+
 intId   = cdr.exNormalize(docId)[1]
 cursor.execute("""\
     SELECT b.data
