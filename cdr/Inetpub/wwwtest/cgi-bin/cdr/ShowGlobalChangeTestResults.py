e6726e025b51a845ac40db85d6d9bd2be928ade1
ameyer <webcommauto@users.noreply.github.com>
2008-05-22 16:53:07 +0000

    <R8196> Added some sort, count, and error reporting capability.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/ShowGlobalChangeTestResults.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/ShowGlobalChangeTestResults.py
@@ -1,29 +1,43 @@
 #----------------------------------------------------------------------
 #
-# $Id: ShowGlobalChangeTestResults.py,v 1.2 2005-05-26 12:19:22 bkline Exp $
+# $Id: ShowGlobalChangeTestResults.py,v 1.3 2008-05-22 16:53:07 ameyer Exp $
 #
 # Web interface for showing changes which would be made by a
 # global change.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2005/05/26 12:19:22  bkline
+# Listed most recent runs at top; changed heading string; fixed unicode
+# display.
+#
 # Revision 1.1  2004/10/25 16:39:12  bkline
 # Web interface to changes from proposed global change job.
 #
 #----------------------------------------------------------------------
 import cgi, glob, cdrcgi, os, xml.dom.minidom, cdr
 
-BASE = 'd:/cdr/GlobalChange/'
-script = 'ShowGlobalChangeTestResults.py'
-fields = cgi.FieldStorage()
+BASE      = 'd:/cdr/GlobalChange/'
+script    = 'ShowGlobalChangeTestResults.py'
+fields    = cgi.FieldStorage()
 directory = fields and fields.getvalue("dir") or None
-file = fields and fields.getvalue("file") or None
+file      = fields and fields.getvalue("file") or None
+sortBy    = fields and fields.getvalue("sortBy") or "byDiff"
+reSort    = fields and fields.getvalue("reSort") or None
+reSortDir = fields and fields.getvalue("reSortDir") or None
 
+# If re-sort requested, set the directory to force re-sorting
+if reSort:
+    directory = reSortDir
+
+# Format an xml file with nice indentation
 def prettyPrint(doc):
     doc = xml.dom.minidom.parseString(doc).toprettyxml("  ")
     if type(doc) == type(u""):
         doc = doc.encode('utf-8')
     return cdr.stripBlankLines(doc)
 
+# If we got here by a hyperlink to a file from a page constructed
+#   by this program, display the file as prettyPrinted xml, or text.
 if file and directory:
     f = open(BASE + directory + "/" + file)
     doc = f.read()
@@ -40,21 +54,44 @@ if file and directory:
 """ % cgi.escape(unicode(doc, "utf-8")))
 
 class Set:
+    # Collection of filenames for one CDR ID
     def __init__(self):
         self.old = None
         self.new = None
         self.diff = None
+        self.err = None
+
 class Doc:
+    # Collection of all files, for all versions, of one xml doc
+    # lastv and lastp may or may not exist
     def __init__(self, id):
         self.id = id
         self.cwd = Set()
         self.lastv = Set()
         self.lastp = Set()
+        self.cwdSize = None
+        self.diffSize = None
 
 def makeLink(name, label):
+    # Construct a full hyperlink to show a file using this program
     return "<a href='%s?dir=%s&file=%s'>%s</a>" % (script, directory, name,
                                                    label)
-def makeRow(id, col2, set):
+
+def makeRow(id, col2, set, doc):
+    """
+    Make one row in the file list display
+    Pass:
+        id   - Text format doc id, or "&nbsp;" if id already shown.
+        col2 - Disply name, one of "CWD", "LASTV", etc.
+        set  - Collection of filenames for this doc id.
+        doc  - Doc object.
+    Return:
+        One complete row for the HTML table of hyperlinked files.
+    """
+    # If there is an errors file, we need to hyperlink it
+    errLink = "&nbsp;"
+    if set.err:
+        errLink = makeLink(set.err, 'Errs')
     return """\
   <tr>
    <td>%s</td>
@@ -62,15 +99,23 @@ def makeRow(id, col2, set):
    <td>%s</td>
    <td>%s</td>
    <td>%s</td>
+   <td>%s</td>
+   <td align='right'>%s</td>
+   <td align='right'>%s</td>
   </tr>
 """ % (id, col2, makeLink(set.old, 'Old'), makeLink(set.new, 'New'),
-       makeLink(set.diff, 'Diff'))
+       makeLink(set.diff, 'Diff'), errLink, doc.cwdSize, doc.diffSize)
+
+# If the user has picked a directory, take him to a display of
+#   all files in that directory
 if directory:
-    files = os.listdir(BASE + directory)
+    baseDir = BASE + directory
+    files = os.listdir(baseDir)
     docs = {}
     for file in files:
         if file.startswith('CDR0') and (file.endswith('xml') or
-                                        file.endswith('diff')):
+                                        file.endswith('diff') or
+                                        file.endswith('NEW_ERRORS.txt')):
             id = file[:13]
             if id not in docs:
                 doc = docs[id] = Doc(id)
@@ -84,24 +129,66 @@ if directory:
                 set = doc.lastv
             if file.endswith('.diff'):
                 set.diff = file
+                if file[13:17] == '.cwd':
+                    doc.diffSize = os.stat(baseDir + '/' + file).st_size
+            if file.endswith('.NEW_ERRORS.txt'):
+                set.err = file
             elif file.endswith('new.xml'):
                 set.new = file
             elif file.endswith('old.xml'):
                 set.old = file
+                if file[13:17] == '.cwd':
+                    doc.cwdSize = os.stat(baseDir + '/' + file).st_size
     keys = docs.keys()
+
+    # Check radio buttons
+    chkSortDiff  = ''
+    chkSortSize  = ''
+    chkSortCdrId = ''
+
+    # Construct an index dictionary of id = Doc
+    sortKeys = {}
+    maxsize  = 10000000  # Subtract from here to reverse order of sort
+    if sortBy == "byCdrId":
+        # Index = id -> id -> Doc
+        # Not needed, but simplifies the logic to have all indexes at the
+        #   same level of indirection
+        for key in keys:
+            sortKeys[key] = key
+        chkSortCdrId = " checked='1'"
+    elif sortBy == "byDiff":
+        # Index = Diff file size -> id -> Doc
+        # Using negative of size to get reverse sort order
+        for key in keys:
+            newKey = '%d.%s' % (maxsize - docs[key].diffSize, key)
+            sortKeys[newKey] = key
+        chkSortDiff = " checked='1'"
+    elif sortBy == "byCwdSize":
+        # Index = CWD file size -> id -> Doc
+        for key in keys:
+            newKey = '%d.%s' % (maxsize - docs[key].cwdSize, key)
+            sortKeys[newKey] = key
+        chkSortSize = " checked='1'"
+
+    keys = sortKeys.keys()
     keys.sort()
     rows = []
     for key in keys:
-        doc = docs[key]
+        doc = docs[sortKeys[key]]
         id = doc.id
         if doc.cwd.diff:
-            rows.append(makeRow(id, "CWD", doc.cwd))
+            rows.append(makeRow(id, "CWD", doc.cwd, doc))
             id = "&nbsp;"
         if doc.lastv.diff:
-            rows.append(makeRow(id, "LASTV", doc.lastv))
+            rows.append(makeRow(id, "LASTV", doc.lastv, doc))
             id = "&nbsp;"
         if doc.lastp.diff:
-            rows.append(makeRow(id, "LASTP", doc.lastp))
+            rows.append(makeRow(id, "LASTP", doc.lastp, doc))
+
+    # Counts
+    docCount = len(keys)
+    verCount = len(rows)
+
     cdrcgi.sendPage("""\
 <html>
  <head>
@@ -111,13 +198,29 @@ if directory:
   </style>
  </head>
  <body>
+<form>
+ <center>
+ <input type='hidden' name='reSortDir' value='%s' />
+ <input type='submit' name='reSort' value='Sort By' />
+ <input type='radio' name='sortBy' value='byCdrId'%s>CDR ID</input>
+ <input type='radio' name='sortBy' value='byDiff'%s>Diff size</input>
+ <input type='radio' name='sortBy' value='byCwdSize'%s>File size</input>
+ </center>
+</form>
+<p>Total docs = %d &nbsp; Total versions = %d</p>
+
   <table border='0'>
+  <tr>
+   <td>CDR ID</td><td>Ver.</td><td colspan='4'>Files</td>
+   <td>CWD Size</td><td>Diff size</td>
+  </tr>
 %s
   </table>
  </body>
 </html>
-""" % "".join(rows))
-    
+""" % (directory, chkSortCdrId, chkSortDiff, chkSortSize,
+       docCount, verCount, "".join(rows)))
+
 dirs =  glob.glob(BASE + '/20*')
 dirs.sort()
 dirs.reverse()
