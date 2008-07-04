3ca6ccb1c300b6546691cafc3d0d54280462d2e6
ameyer <webcommauto@users.noreply.github.com>
2008-07-04 02:57:21 +0000

    <R8264> Added menu entries for saving old version as CWD, replacing Summary.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/DevSA.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/DevSA.py
@@ -1,10 +1,13 @@
 #----------------------------------------------------------------------
 #
-# $Id: DevSA.py,v 1.6 2007-10-31 16:03:33 bkline Exp $
+# $Id: DevSA.py,v 1.7 2008-07-04 02:57:21 ameyer Exp $
 #
 # Main menu for Developers and System Administrators.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.6  2007/10/31 16:03:33  bkline
+# Added republish command.
+#
 # Revision 1.5  2006/01/19 21:12:24  ameyer
 # Added ProjectedAccrual processing to menu.  May remove it later.
 #
@@ -66,6 +69,8 @@ items   = (('EditGroups.py',           'Manage Groups'                 ),
            ('EditExternMap.py',        'Update Mapping Table'          ),
            ('Request1931.py',
             'Guess ExpectedEnrollment from ProjectedAccrual'),
+           ('ReplaceCWDwithVersion.py','Replace CWD with Older Version'),
+           ('ReplaceDocWithNewDoc.py', 'Replace Doc with New Doc'      ),
            ('Logout.py',               'Log Out'                       )
            )
 for item in items:
