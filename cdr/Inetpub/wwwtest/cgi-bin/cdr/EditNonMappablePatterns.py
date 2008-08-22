51b81516ba1bbfea1820867976d03d4792d01759
ameyer <webcommauto@users.noreply.github.com>
2008-08-22 04:00:57 +0000

    <R8365> Added ability to review all existing external_map strings and make any non-mappable that match the patterns. Also made a number of user interface tweaks.

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/EditNonMappablePatterns.py
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/EditNonMappablePatterns.py
@@ -5,16 +5,19 @@
 # expressions (in SQL "LIKE" format) for CTGov Facility values that
 # cannot be mapped.
 #
-# $Id: EditNonMappablePatterns.py,v 1.2 2008-08-20 03:52:04 ameyer Exp $
+# $Id: EditNonMappablePatterns.py,v 1.3 2008-08-22 04:00:57 ameyer Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2008/08/20 03:52:04  ameyer
+# Numerous changes, had to replace simple URL link with javascript.
+#
 # Revision 1.1  2008/07/23 05:08:56  ameyer
 # Initial version.
 #
 #----------------------------------------------------------------------
 import cgitb; cgitb.enable()
 
-import cgi, cdrcgi, cdrdb, cdr
+import cgi, cdrcgi, cdrdb, cdr, extMapPatChk
 
 # Use same logfile used for changing values
 LF=cdr.DEFAULT_LOGDIR + "/GlobalChangeCTGovMapping.log"
@@ -22,7 +25,7 @@ LF=cdr.DEFAULT_LOGDIR + "/GlobalChangeCTGovMapping.log"
 # Constants
 MAINTITLE          = "Maintain CTGov non-Mapping Patterns"
 SCRIPT             = "EditNonMappablePatterns.py"
-MAX_DISPLAY_VALUES = 5000
+MAX_DISPLAY_VALUES = 6000
 
 #----------------------------------------------------------------------
 #  Subroutines
@@ -68,24 +71,44 @@ def showInitialScreen(session):
         document.forms[0].Request.value = addOrView;
         document.forms[0].newAction.value = addOrView;
         document.forms[0].submit();
-        // var url="EditNonMappablePatterns.py?%s=" +
-        //          document.getElementById("session").value +
-        //          "&newAction=" + addOrView + "&newPattern=" +
-        //          document.getElementById("newPattern").value;
-        // location.href=url;
     }
  }
 </script>
-""" % cdrcgi.SESSION)
+""")
 
     html += """
-<p>To create a new pattern, enter it, using SQL "LIKE" semantics, in the
-input box and click the "<em>Add</em>" button to add it, or the
-<em>View</em> button to just view matching values.</p>
+<h3>Mark unmappable facilities Non-mappable</h3>
+<p>Click <em>Apply</em> to search all CT.gov Facility strings for
+values matching non-mappable patterns and mark them as non-mappable.</p>
+<p>Click <em>View</em> to just view a list of currently mappable strings
+that should be made non-mappable.</p>
+
+<center>
+<table border='0' cellspacing='15'>
+ <tr>
+  <td><input type='submit' name='applyPatterns' value='Apply' /></td>
+  <td><input type='submit' name='applyPatterns' value='View' /></td>
+ </tr>
+</table>
+</center>
+
+<hr />
+
+<h3>Update the Non-mappable Pattern Table</h3>
+
+<p>To create a new pattern, enter a string using '_' to stand for
+any one character and '%%' for any sequence of 0 or more characters, then
+click the "<em>Add</em>" button to add it to the list of non-mappable
+patterns, or the <em>View</em> button to just view matching values.</p>
+
+<p>Use '\\_' or '\\%%' for literal underscore and percent signs.  Patterns
+are not case sensitive.</p>
 
 <p>There are currently %d existing patterns.
 To remove one, click the "<em>Del</em>" link for that pattern or the
 <em>View</em> link to just view matching values.<p>
+
+<p>To edit a pattern, Add the correct value and Delete the incorrect one.</p>
 
 <table border="2">
  <tr>
@@ -116,6 +139,129 @@ To remove one, click the "<em>Del</em>" link for that pattern or the
 """ % (cdrcgi.SESSION, session)
 
     # Send it to the browser, completing this interaction
+    cdrcgi.sendPage(html)
+
+def propagatePatterns(session, runMode):
+    """
+    Load all the patterns in the external_map_nomap_pattern table,
+    and check every single external map value against them.
+
+    Optionally update any matching values in the external_map table.
+
+    Heavy lifting is done in the ExternalMapPatternCheck object.
+
+    Pass:
+        runMode - 'test' = just report results.
+                  'run'  = update the database and report results.
+    """
+    cdr.logwrite("Applying patterns")
+    # DEBUG runMode = "test"
+    # Instantiate an object that does everything
+    try:
+        noMapObj = extMapPatChk.ExternalMapPatternCheck(session=session)
+    except cdr.Exception, info:
+        # Failed
+        cdrcgi.bail("Oops.  ExternalMapPatternCheck reports: %s" % info)
+
+    # If update requested
+    if runMode == 'run':
+        try:
+            noMapObj.updateDatabase()
+        except cdr.Exception, info:
+            # Failed
+            cdrcgi.bail("Error updating the database: %s" % info)
+
+    # Show what happened or would happen
+    showNomapReport(noMapObj, runMode)
+
+def showNomapReport(noMapObj, runMode):
+    """
+    Display a screen showing what did or would happen as a result of
+    calling propagatePatterns().
+
+    Pass:
+        noMapObj - ExternalMapPatternCheck object.
+        runMode  - 'test' or 'run'
+    """
+    cdr.logwrite("Showing report for Applying patterns")
+    buttons = (cdrcgi.MAINMENU, "Log Out")
+    html = cdrcgi.header(MAINTITLE, MAINTITLE, "Results of ApplyPatterns",
+                         script=SCRIPT, buttons=buttons)
+
+    # Get summary counts
+    chkCount    = noMapObj.getCheckedCount()
+    matchCount  = noMapObj.getMatchedCount()
+    updateCount = noMapObj.getUpdatedCount()
+    mapCount    = noMapObj.getMappedCount()
+
+    # Show update results
+    html += """
+<h2>Results of applying patterns</h2>
+<h3>Summary</h3>
+<table border='1'>
+ <tr>
+  <td>%d</td>
+  <td>Mappable CT.gov Facility patterns were checked</td>
+ </tr>
+ <tr>
+  <td>%d</td>
+  <td>Values matched a non-mappable pattern</td>
+ </tr>
+ <tr>
+  <td>%d</td>
+  <td>Values were updated</td>
+ </tr>
+ <tr>
+  <td>%d</td>
+  <td>Values were already mapped and cannot be updated by this program</td>
+ </tr>
+</table>
+
+<h3>Details</h3>
+
+""" % (chkCount, matchCount, updateCount, mapCount)
+
+    if mapCount > 0:
+        mappedRows = noMapObj.getMappedValues()
+        html += """
+<p><strong>Non-mappable values that were already mapped</strong></p>
+
+<table border='1'>
+ <tr>
+  <td><strong>Doc ID</strong></td>
+  <td><strong>CT.gov Facility string</strong></td>
+ </tr>
+"""
+        for row in mappedRows:
+            html += """
+ <tr>
+  <td>%d</td><td>%s</td>
+ </tr>
+""" % (row[0], row[1])
+        html += "</table>\n"
+
+    if matchCount:
+        # Show items that were or would have been marked non-mappable
+        if runMode == 'test':
+            html += """
+<p><strong>The following values would have been updated</strong></p>
+"""
+        else:
+            html += """
+<p><strong>The following values were marked non-mappable</strong></p>
+"""
+        values = noMapObj.getValues()
+        html += """
+<table border='1'>
+ <tr><td><strong>CT.gov Facility string</strong></td></tr>
+"""
+        for value in values:
+            html += " <tr><td>%s</td></tr>\n" % value
+        html += "</table>\n"
+
+    html += """
+<p>Click the back button or a navigation bar button to continue</p>
+"""
     cdrcgi.sendPage(html)
 
 def checkPattern(action, session, patternId=None, pattern=None):
@@ -386,13 +532,13 @@ def updateDB(session, oldId=None, newPattern=None):
             cdrcgi.bail("Error deleting old pattern: %s" % str(info))
 
     # Tell user
-    buttons = ("Another", cdrcgi.MAINMENU, "Log Out")
+    buttons = ("Continue", cdrcgi.MAINMENU, "Log Out")
     html = cdrcgi.header(MAINTITLE, MAINTITLE, "Update completed",
                          script=SCRIPT, buttons=buttons)
 
     html += """
-<p>The update is complete, click "Another" above to perform another
-pattern update</p>
+<p>The update is complete, click "Continue" above to return to the main
+screen</p>
 <input type="hidden" name="%s" value="%s" />
 <input type="hidden" name="newAction" />
 </form>
@@ -428,20 +574,21 @@ if request in (cdrcgi.MAINMENU, "Cancel"):
     cdrcgi.navigateTo ("Admin.py", session)
 elif request == "Log Out":
     cdrcgi.logout(session)
-elif request == "Another":
+elif request == "Continue":
     # Start all over again as if coming in for the first time
     request = None
 
 # Get relevant variables from last viewed form
-oldId      = fields.getvalue("oldId", None)
-newPattern = fields.getvalue("newPattern", None)
-newAction  = fields.getvalue("newAction", None)
+oldId         = fields.getvalue("oldId", None)
+newPattern    = fields.getvalue("newPattern", None)
+newAction     = fields.getvalue("newAction", None)
+applyPatterns = fields.getvalue("applyPatterns", None)
 
-# cdr.logwrite("Top: oldId=%s, newPattern=%s, newAction=%s" %
-#               (oldId, newPattern, newAction))
+cdr.logwrite("request=%s oldId=%s newPattern=%s applyPatterns=%s newAction=%s" %
+(request, oldId, newPattern, applyPatterns, newAction))
 
 # Is this the first time we're in the function?
-if not request and not newAction:
+if not (request or newAction or applyPatterns):
     # Show screen and exit
     showInitialScreen(session)
 
@@ -450,6 +597,13 @@ if request in ("Confirm Add", "Confirm Delete"):
     updateDB(session, oldId, newPattern)
 
 # Perform requested action
+if applyPatterns:
+    if applyPatterns == "Apply":
+        runMode = "run"
+    else:
+        runMode = "test"
+    propagatePatterns(session, runMode)
+
 if newAction == "add":
     if not newPattern:
         cdrcgi.bail("You must first create a new pattern to add it")
