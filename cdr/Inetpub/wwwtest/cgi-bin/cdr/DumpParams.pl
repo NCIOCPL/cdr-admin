3fcebdf0f355777a0f4b0dc61a94a7a78822939d
Bob Kline <bkline@rksystems.com>
2008-09-02 19:33:56 +0000

    <R8401> Changed dump to Dump (tracking change to module).

--- cdr/Inetpub/wwwtest/cgi-bin/cdr/DumpParams.pl
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/DumpParams.pl
@@ -7,6 +7,6 @@ print "Content-type: text/html\n\n";
 print "<H2>Current Values</H2> $query\n<HR>\n";
 #print "<HTML><HEAD><TITLE>Parameter list</TITLE></HEAD><BODY>\n";
 #foreach $keyword (@keywords) { print "$keyword<BR>\n"; }
-print $query->dump;
+print $query->Dump;
 print "</BODY></HTML>\n";
 # @values = $query->param('foo');
