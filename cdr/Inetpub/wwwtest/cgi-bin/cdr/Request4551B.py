96d2d9e7d4f5e76db3386f8fc90318b6430518c7
Volker Englisch <venglisch@users.noreply.github.com>
2009-04-29 00:14:07 +0000

    <R8911> Program which reads two spreadsheets and identifies if the NCT-IDs listed on the first spreadsheet can be found on the second. (Bug 4551)

---
+++ cdr/Inetpub/wwwtest/cgi-bin/cdr/Request4551B.py
@@ -0,0 +1,403 @@
+#----------------------------------------------------------------------
+#
+# $Id: Request4551B.py,v 1.1 2009-04-29 00:14:07 venglisc Exp $
+#
+# Update Non-Compliance Report from CTGov.
+#
+# $Log: not supported by cvs2svn $
+#
+#
+#----------------------------------------------------------------------
+import cdr, cdrdb, sys, time, cdrdocobject #, cdrcgi, 
+import xml.dom.minidom, ExcelWriter, ExcelReader
+
+if sys.platform == "win32":
+    import os, msvcrt
+    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+
+conn = cdrdb.connect('CdrGuest')
+#conn.setAutoCommit()
+cursor = conn.cursor()
+
+# -------------------------------------------------------------------
+# Protocol class to identify the elements requested
+# -------------------------------------------------------------------
+class Protocol:
+    def __init__(self, nctInfo, cursor):
+        self.cdrId     = nctInfo[u'CDRID']
+        self.closed    = nctInfo[u'Compl before?']
+        self.complDate = nctInfo[u'Compl Date']
+        self.completed = None
+        self.respp     = None
+        self.fdaReg    = u'No'
+        self.safety    = u'No'
+        self.arms      = u'No'
+        self.usSites   = None
+        self.phase     = []
+        self.status    = None
+        self.source    = []
+        self.primaryId = None
+
+        # Extract the XML document and create the DOM tree
+        # ------------------------------------------------
+        cursor.execute("""\
+            SELECT xml
+              FROM pub_proc_cg
+             WHERE id = ? """, (self.cdrId))
+        try:
+            docXml = cursor.fetchall()[0][0]
+        except:
+            print 'No document in CG table for CDR%s' % self.cdrId
+            docXml = None
+            return
+            
+
+        dom = xml.dom.minidom.parseString(docXml.encode('utf-8'))
+
+        # Setting initial variables
+        # -------------------------
+        multiArms = 0
+        singleArm = None
+        safety = allSafety = None
+        ynFda  = None
+        respp = []
+
+        # Walking through the tree to find the elements available in the 
+        # licensee output
+        # --------------------------------------------------------------
+        for node in dom.documentElement.childNodes:
+            # Find the value of the Protocol ID
+            # ---------------------------------
+            if node.nodeName == 'ProtocolIDs':
+                for child in node.childNodes:
+                    if child.nodeName == 'PrimaryID':
+                        self.primaryId = getElementText(child, 'IDString')
+            # Identify if the FDARegulated element exists and is not empty
+            # ------------------------------------------------------------
+            elif node.nodeName == 'RegulatoryInformation':
+                ynFda = getElementText(node, 'FDARegulated')
+
+                # Identify if Responsible Party exists with children
+                # --------------------------------------------------
+                for child in node.childNodes:
+                    if child.nodeName == 'ResponsibleParty':
+                        for nextChild in child.childNodes:
+                            if nextChild.nodeName in ('ResponsiblePerson',
+                                                    'ResponsibleOrganization'):
+                                for next2Child in nextChild.childNodes:
+                                    respp.append(cdr.getTextContent(next2Child))
+            elif node.nodeName == 'ProtocolAbstract':
+                for child in node.childNodes:
+                    if child.nodeName == 'Professional':
+                        for nextChild in child.childNodes:
+                            # Identify if an empty ArmsOrGroups element exists
+                            # with the SingleArm attribute or none-empty
+                            # ArmOrGroup elements exist.
+                            # ------------------------------------------------
+                            if nextChild.nodeName == 'ArmsOrGroups':
+                                singleArm = nextChild.getAttribute(
+                                              'SingleArmOrGroupStudy')
+                                for next2Child in nextChild.childNodes:
+                                    if next2Child.nodeName == 'ArmOrGroup':
+                                        multiArms += 1
+                            # Identify that all Outcome elements exist with 
+                            # the Saftey attribute
+                            # ---------------------------------------------
+                            elif nextChild.nodeName == 'Outcome':
+                                safety = nextChild.getAttribute(
+                                            'Safety') or None
+                                if not safety and not allSafety:
+                                    allSafety = u'No'
+
+            # Extract the ProtocolPhase elements
+            # ----------------------------------
+            elif node.nodeName == 'ProtocolPhase':
+                self.phase.append(cdr.getTextContent(node))
+            # Extract the protocol status
+            # ---------------------------
+            elif node.nodeName == 'ProtocolAdminInfo':
+                self.status = getElementText(node, 'CurrentProtocolStatus')
+                country = []
+                for child in node.childNodes:
+                    if child.nodeName == 'ProtocolSites':
+                        country = dom.documentElement.getElementsByTagName(
+                                                               'CountryName')
+
+        isUsTrial = None
+        for node in country:
+            # print node.nodeName, cdr.getTextContent(node)
+            if cdr.getTextContent(node) == 'U.S.A.':
+                isUsTrial = 'Yes'
+                break
+
+
+
+        # Setting the ResponsibleParty Y/N flag
+        # -------------------------------------
+        self.completed = None
+        if len(respp):
+           self.respp     = u'Yes'
+        else:
+           self.respp     = u'No'
+        nctInfo[u'respp'] = self.respp
+        
+        # Setting the FDA Regulated Y/N flag
+        # ----------------------------------
+        if ynFda:
+            self.fdaReg = u'Yes'
+        nctInfo[u'fdaReg'] = self.fdaReg
+
+        # Setting the Outcome with Saftey Y/N flag
+        # ----------------------------------------
+        if allSafety:
+            self.safety = u'No'
+        else:
+            self.safety = u'Yes'
+        nctInfo[u'safety'] = self.safety
+
+        # Setting the ArmsOrGroups Y/N flag
+        # ---------------------------------
+        if singleArm or multiArms > 0:
+            self.arms  = u'Yes'
+        nctInfo[u'arms'] = self.arms
+
+        if isUsTrial:
+            nctInfo[u'us'] = u'Yes'
+
+        # Setting the phase and status
+        # ----------------------------
+        nctInfo[u'phase'] = self.phase
+        nctInfo[u'status'] = self.status
+
+        # Extracting the source name from the database since this information
+        # is not part of the vendor output
+        # -------------------------------------------------------------------
+        cursor.execute("""\
+            SELECT value 
+              FROM query_term_pub
+             WHERE PATH = '/InScopeProtocol/ProtocolSources' +
+                          '/ProtocolSource/SourceName'
+               AND doc_id = ? """, (self.cdrId))
+        rows = cursor.fetchall()
+        if rows:
+            for row in rows:
+                self.source.append(row[0])
+        nctInfo[u'source'] = self.source
+
+        # Setting the protocol ID
+        # -----------------------
+        nctInfo[u'pid'] = self.primaryId
+
+
+
+def getElementText(parent, name):
+    nodes = parent.getElementsByTagName(name)
+    return nodes and cdr.getTextContent(nodes[0]) or None
+
+
+# -------------------------------------------------------------
+# Read the spreadsheet from Kim (CDR-list)
+# -------------------------------------------------------------
+def readCdrList(filename = 'd:/cdr/tmp/CDR_Protocols.xls'):
+    cdrBook = ExcelReader.Workbook(filename)
+    sheet = cdrBook[6]
+    headerRow = 1
+    rownum = 0
+    cdrProtocols = {}
+
+    n = 0
+
+    for row in sheet.rows:
+        n += 1
+        nctId      = row[0]
+        cdrId      = row[1]
+        cdrProtocols[nctId.val] = {u'CDRID':str(cdrId)}
+
+        #if n > 10:
+        #    return cdrProtocols
+
+    return cdrProtocols
+
+
+# -------------------------------------------------------------
+# Read the spreadsheet from NLM (NCT-list)
+# -------------------------------------------------------------
+def readNctList(filename = 'd:/cdr/tmp/NLM_Protocols.xls'):
+    nctBook = ExcelReader.Workbook(filename)
+    sheet = nctBook[0]
+    headerRow = 9
+    rownum = 0
+    hasOutcomes     = []
+    hasStartDate    = []
+    hasEnrollment   = [] 
+    hasNumArms      = [] 
+    hasRespParty    = [] 
+    hasSafety       = [] 
+    hasFda          = [] 
+    hasEndDate      = [] 
+    hasEndDatePrime = [] 
+    
+    i = 0
+    for row in sheet.rows:
+        i += 1
+        if i > 5:
+            if row[4]: hasOutcomes.append(row[4].val)
+            if row[5]: hasStartDate.append(row[5].val)
+            if row[6]: hasEnrollment.append(row[6].val)
+            if row[7]: hasNumArms.append(row[7].val)
+            if row[8]: hasRespParty.append(row[8].val)
+            if row[9]: hasSafety.append(row[9].val)
+            if row[10]: hasFda.append(row[10].val)
+            if row[11]: hasEndDate.append(row[11].val)
+            if row[12]: hasEndDatePrime.append(row[12].val)
+
+        #if i > 10:
+        #    return (hasOutcomes, hasStartDate, hasEnrollment, hasNumArms, 
+        #        hasRespParty, hasSafety, hasFda, hasEndDate, hasEndDatePrime)
+
+    return (hasOutcomes, hasStartDate, hasEnrollment, hasNumArms, 
+            hasRespParty, hasSafety, hasFda, hasEndDate, hasEndDatePrime)
+
+
+# Excel is able to read XML files so that's what we create here
+# -------------------------------------------------------------
+quickTitle = "Compare CDR and NCT Report"
+t = time.strftime("%Y%m%d%H%M%S")
+REPORTS_BASE = 'd:/cdr/tmp'
+#REPORTS_BASE = 'm:/cdr/tmp'
+name = '/CompareCdrNct-%s.xml' % t
+cdrName = '/test_cdr.xls'
+nctName = '/test_nct.xls'
+fullname = REPORTS_BASE + name
+
+# ----------------------------------------------------------------------
+# First Step:
+# We need to read the content of the Spreadsheets provided
+# ----------------------------------------------------------------------
+cdrList = readCdrList(filename = REPORTS_BASE + cdrName)
+nctList = readNctList(filename = REPORTS_BASE + nctName)
+
+### conn = cdrdb.connect('CdrGuest')
+### cursor = conn.cursor()
+
+#print '**************'
+#print cdrList
+#print '**************'
+#print nctList[0]
+
+# Split the NCT list into the distinct columns to search 
+# ------------------------------------------------------
+hasOutcomes     = nctList[0]
+hasStartDate    = nctList[1]
+hasEnrollment   = nctList[2]
+hasNumArms      = nctList[3]
+hasRespParty    = nctList[4]
+hasSafety       = nctList[5]
+hasFda          = nctList[6]
+hasEndDate      = nctList[7]
+hasEndDatePrime = nctList[8]
+
+#sys.exit(0)
+
+
+# Create the spreadsheet and define default style, etc.
+# -----------------------------------------------------
+wb      = ExcelWriter.Workbook()
+b       = ExcelWriter.Border()
+borders = ExcelWriter.Borders(b, b, b, b)
+font    = ExcelWriter.Font(name = 'Times New Roman', size = 11)
+align   = ExcelWriter.Alignment('Left', 'Top', wrap = True)
+style1  = wb.addStyle(alignment = align, font = font)
+# style1  = wb.addStyle(alignment = align, font = font, borders = borders)
+urlFont = ExcelWriter.Font('blue', None, 'Times New Roman', size = 11)
+style4  = wb.addStyle(alignment = align, font = urlFont)
+ws      = wb.addWorksheet(quickTitle, style1, 45, 1)
+style2  = wb.addStyle(alignment = align, font = font, 
+                         numFormat = 'YYYY-mm-dd')
+alignH  = ExcelWriter.Alignment('Left', 'Bottom', wrap = True)
+headFont= ExcelWriter.Font(bold=True, name = 'Times New Roman', size = 12)
+styleH  = wb.addStyle(alignment = alignH, font = headFont)
+    
+# Set the colum width
+# -------------------
+ws.addCol( 1,  90)
+ws.addCol( 2,  55)
+ws.addCol( 3,  60)
+ws.addCol( 4,  60)
+ws.addCol( 5,  60)
+ws.addCol( 6,  60)
+ws.addCol( 7,  60)
+ws.addCol( 8,  60)
+ws.addCol( 9,  60)
+ws.addCol(10,  60)
+ws.addCol(11,  60)
+
+# Create the Header row
+# ---------------------
+exRow = ws.addRow(1, styleH)
+exRow.addCell(1, 'NCT-ID')
+exRow.addCell(2, 'CDR-ID')
+exRow.addCell(3, 'Has Outcomes')
+exRow.addCell(4, 'Has Start Date')
+exRow.addCell(5, 'Has Enrollment')
+exRow.addCell(6, 'Has Number of Arms')
+exRow.addCell(7, 'Has Resp Party')
+exRow.addCell(8, 'Has Safety Issue')
+exRow.addCell(9, 'Has FDA Regulated')
+exRow.addCell(10, 'Has End Date')
+exRow.addCell(11, 'Has End Date Prime')
+
+# Add the protocol data one record at a time beginning after 
+# the header row
+# ----------------------------------------------------------
+rowNum = 1
+for row in cdrList.keys():
+    if cdrList[row][u'CDRID'] != 'CDR ID':
+        rowNum += 1
+        exRow = ws.addRow(rowNum, style1, 40)
+        exRow.addCell(1, row)
+        exRow.addCell(2, cdrList[row][u'CDRID'])
+        if row in hasOutcomes:
+            exRow.addCell(3, 'No')
+        if row in hasStartDate:
+            exRow.addCell(4, 'No')
+            #print 'No StartDate: %s' % row
+        if row in hasEnrollment:
+            exRow.addCell(5, 'No')
+            #print 'No Enrollment: %s' % row
+        if row in hasNumArms:
+            exRow.addCell(6, 'No')
+            #print 'No NumArms: %s' % row
+        if row in hasRespParty:
+            exRow.addCell(7, 'No')
+            #print 'No RespParty: %s' % row
+        if row in hasSafety:
+            exRow.addCell(8, 'No')
+            #print 'No Safety: %s' % row
+        if row in hasFda:
+            exRow.addCell(9, 'No')
+            #print 'No hasFda: %s' % row
+        if row in hasEndDate:
+            exRow.addCell(10, 'No')
+            #print 'No EndDate: %s' % row
+        if row in hasEndDatePrime:
+            exRow.addCell(11, 'No')
+            #print 'No EndDatePrime: %s' % row
+
+
+# # Web report
+# # ----------
+# print "Content-type: application/vnd.ms-excel"
+# print "Content-Disposition: attachment; filename=ContentInventory-%s.xls" % t
+# print  
+# 
+# wb.write(sys.stdout, True)
+
+# Save the Report
+# ---------------
+fobj = file(fullname, "w")
+wb.write(fobj)
+print ""
+print "  Report written to %s" % fullname
+fobj.close()
+
