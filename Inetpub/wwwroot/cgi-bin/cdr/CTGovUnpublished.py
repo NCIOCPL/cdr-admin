d9daab7b79b7cf26a67a55bd8ac0e4a6ce35ec56
Bob Kline <bkline@rksystems.com>
2009-10-21 18:34:47 +0000

    <R9303> Modified logic to ignore publishable versions of the document when those versions are of type InScopeProtocol; changed "Date Created" column to show the date a CTGovProtocol version was first created.

--- Inetpub/wwwroot/cgi-bin/cdr/CTGovUnpublished.py
+++ Inetpub/wwwroot/cgi-bin/cdr/CTGovUnpublished.py
@@ -4,12 +4,10 @@
 #
 # Report of unpublished CTGov trials by Phase.
 # The query originated from the AdHoc query interface.
+# Original script written by Volker.
 #
-# $Log: not supported by cvs2svn $
-# Revision 1.1  2006/04/26 17:19:59  venglisc
-# Initial copy of report listing unpublished CTGov protocols by phase.
-# (Bug 2048)
-#
+# BZIssue::2048
+# BZIssue::4666
 #
 #----------------------------------------------------------------------
 import cdr, cdrdb, time, cgi, cdrcgi
@@ -24,24 +22,34 @@ cursor = conn.cursor()
 # Gather the report data.
 #----------------------------------------------------------------------
 cursor.execute("""\
-SELECT d.id CDRID, q.value Phase, min(a.dt) DateCreated, t.value
-  FROM document d
-  JOIN query_term q
-    ON d.id = q.doc_id
-  JOIN audit_trail a
-    ON d.id = a.document
-  LEFT OUTER JOIN query_term t
-    ON d.id = t.doc_id
-   AND t.path = '/CTGovProtocol/IDInfo/OrgStudyID'
- WHERE d.doc_type = 34  -- CTGovProtocol
-   AND d.active_status = 'A'
-   AND q.path = '/CTGovProtocol/Phase'
-   AND q.doc_id NOT IN (SELECT DISTINCT id
-                          FROM doc_version
-                         WHERE publishable = 'Y'
-                       )
- GROUP BY d.id, q.value, t.value
- ORDER BY q.value desc, d.id""")
+    SELECT DISTINCT v.id
+               INTO #p
+               FROM doc_version v
+               JOIN doc_type t
+                 ON t.id = v.doc_type
+              WHERE t.name = 'CTGovProtocol'
+                AND v.publishable = 'Y'""")
+cursor.execute("""\
+    SELECT v.id, MIN(v.dt) AS dt
+      INTO #c
+      FROM doc_version v
+      JOIN doc_type t
+        ON t.id = v.doc_type
+     WHERE t.name = 'CTGovProtocol'
+  GROUP BY v.id""")
+cursor.execute("""\
+         SELECT a.id, p.value, #c.dt, i.value
+           FROM active_doc a
+           JOIN query_term p
+             ON p.doc_id = a.id
+           JOIN #c
+             ON #c.id = a.id
+LEFT OUTER JOIN query_term i
+             ON i.doc_id = a.id
+            AND i.path = '/CTGovProtocol/IDInfo/OrgStudyID'
+          WHERE p.path = '/CTGovProtocol/Phase'
+            AND a.id NOT IN (SELECT id FROM #p)
+       ORDER BY p.value, a.id""")
 rows = cursor.fetchall()
 
 #----------------------------------------------------------------------
