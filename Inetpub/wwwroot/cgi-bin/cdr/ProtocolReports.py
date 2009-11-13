dae763a7db6bf5a24ad9f2c3170f61146c7106f1
Volker Englisch <venglisch@users.noreply.github.com>
2009-11-13 17:56:27 +0000

    <R9369> Adding the menu option for Protocol Transfer by Org/Status. (Bug 4659)

--- Inetpub/wwwroot/cgi-bin/cdr/ProtocolReports.py
+++ Inetpub/wwwroot/cgi-bin/cdr/ProtocolReports.py
@@ -4,120 +4,6 @@
 #
 # Submenu for protocol reports.
 #
-# $Log: not supported by cvs2svn $
-# Revision 1.38  2008/09/02 20:09:10  bkline
-# Added Protocols Linked to Terms report (#4263).
-#
-# Revision 1.37  2008/08/13 19:52:59  bkline
-# Fixed typo (leftover from cut/paste of oncore report); added report
-# of Protocols with Regulatory Info Block.
-#
-# Revision 1.36  2008/07/21 20:20:40  bkline
-# Added new Oncore reports at Kim's request (#4141).
-#
-# Revision 1.35  2008/07/21 17:26:18  bkline
-# Added CTEP reports.
-#
-# Revision 1.34  2008/04/17 18:43:25  bkline
-# Added Non-Drug/Agent Protocol Interventions report.
-#
-# Revision 1.33  2007/10/22 15:58:10  bkline
-# Restructuring for Sheri (request 3700).
-#
-# Revision 1.32  2007/07/31 12:12:52  bkline
-# Added NCIC import report.
-#
-# Revision 1.31  2007/06/12 16:53:10  kidderc
-# 3306. Create a Warehouse Box Number Report.
-#
-# Revision 1.29  2006/11/29 16:04:36  bkline
-# Plugged in two new reports (for requests #2443 and #2513).
-#
-# Revision 1.28  2006/08/31 14:39:39  venglisc
-# Replaced report 'Active Lead Orgs without Active Sites' with the new report
-# 'Active Status without Sites'. (Bug 2379)
-#
-# Revision 1.27  2006/07/11 13:41:50  bkline
-# Added flavor showing title.
-#
-# Revision 1.26  2006/05/10 16:36:32  venglisc
-# I can't spell MS-Excel.  That's fixed now.
-#
-# Revision 1.25  2006/05/10 16:35:06  venglisc
-# Added menu item for Protocols with FDA IND information report. (Bug 2028)
-#
-# Revision 1.24  2006/05/04 14:26:33  bkline
-# Three new reports added.
-#
-# Revision 1.22  2005/11/18 03:48:54  bkline
-# Plugged in Alan's new CTGov protocols.
-#
-# Revision 1.21  2005/11/18 03:46:07  bkline
-# Plugged in COG import report and two new OSP reports.
-#
-# Revision 1.20  2005/07/07 15:42:32  venglisc
-# Added menu item for protocols with published results report. (Bug 1735)
-#
-# Revision 1.19  2005/06/30 21:55:39  bkline
-# Changed string for NCI Clinical Trials Statistics report.
-#
-# Revision 1.18  2005/06/07 15:49:54  bkline
-# Two new reports plugged in: CTEP orgs without phones and NCI trials.
-#
-# Revision 1.17  2005/05/12 14:49:38  bkline
-# New report for Sheri (request #1684).
-#
-# Revision 1.16  2005/05/11 20:57:24  bkline
-# Report created for Sheri (request #1669).
-#
-# Revision 1.15  2005/04/21 21:27:25  venglisc
-# Added menu option to allow Publish Preview QC report. (Bug 1531)
-#
-# Revision 1.14  2005/03/16 17:20:11  venglisc
-# Corrected some problems in the queries to eliminate incorrect hits.
-# Added another worksheet to include new CTGovProtocols.
-# Modified the output file from HotFixReport to InterimUpdateReport.
-# Added the prtocol type to the list of removed protocols. (Bugs 1396, 1538)
-#
-# Revision 1.13  2004/09/17 14:06:50  venglisc
-# Fixed list items to properly teminate the anker link.
-#
-# Revision 1.12  2004/07/13 17:59:21  bkline
-# Replaced Newly Published Trials with Newly Publishable Trials.
-#
-# Revision 1.11  2004/05/11 17:38:36  bkline
-# Plugged in protocol hotfix report.
-#
-# Revision 1.10  2004/02/17 19:13:33  venglisc
-# Removed unused Menu entries and alphabetized remaining entries.
-#
-# Revision 1.9  2003/05/08 20:24:01  bkline
-# New report for Office of Science Policy.
-#
-# Revision 1.8  2003/03/06 20:55:04  bkline
-# Fixed typo (Orrice -> Office).
-#
-# Revision 1.7  2003/03/06 20:54:03  bkline
-# Added back in two menu items which had disappeared.
-#
-# Revision 1.6  2003/03/04 22:46:58  bkline
-# Modifications for CDR enhancement request #301.
-#
-# Revision 1.5  2003/01/29 18:46:25  bkline
-# New report on protocols with status change entered in a given date range.
-#
-# Revision 1.4  2003/01/22 23:28:30  bkline
-# New report for issue #560.
-#
-# Revision 1.3  2002/09/23 17:36:40  bkline
-# New report for European protocols.
-#
-# Revision 1.2  2002/05/25 02:39:13  bkline
-# Removed extra blank lines from HTML output.
-#
-# Revision 1.1  2002/05/24 20:37:31  bkline
-# New Report Menu structure implemented.
-#
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, string, time
 
@@ -199,7 +85,9 @@ for r in (('ApprovedNotYetActive.py',
           ('ProtSitesWithoutPhone.py',
            'Protocol Sites Without Phone Numbers', ''),
           ('ProtOwnershipTransfer.py',
-           'Protocol Transfer of Ownership Responses', ''),
+           'Protocol Transfer of Ownership Responses (Batch Job)', ''),
+          ('ProtOwnershipTransferOrg.py',
+           'Protocol Transfer of Ownership Responses by Org/Status', ''),
           ('ProtocolsLinkedToTerms.py',
            'Protocols Linked to Terms', ''),
           ('ProtocolActiveNoSites.py',
