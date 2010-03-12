4949f019e49fcdaca73eb09395b5fa5a2e162b18
Bob Kline <bkline@rksystems.com>
2010-03-12 19:25:39 +0000

    <R9522> adding missing closing anchor tag

--- Inetpub/wwwroot/cgi-bin/cdr/Reports.py
+++ Inetpub/wwwroot/cgi-bin/cdr/Reports.py
@@ -2,86 +2,7 @@
 #
 # $Id$
 #
-# Prototype for editing CDR linking tables.
-#
-# $Log: not supported by cvs2svn $
-# Revision 1.25  2007/03/30 15:17:53  venglisc
-# Removed the date from the documentation link since this menu item now
-# calls a sub-menu that already lists the date the documentation was
-# created.
-#
-# Revision 1.24  2007/01/05 23:23:11  venglisc
-# Added new menu item for Publishing. (Bug 2111)
-#
-# Revision 1.23  2006/09/28 23:08:52  ameyer
-# Changed date and message for Documentation link.
-#
-# Revision 1.22  2006/05/04 14:34:13  bkline
-# Added Drug information reports.
-#
-# Revision 1.21  2005/05/04 18:11:33  venglisc
-# Added menu option for Media QC Reports. (Bug 1653)
-#
-# Revision 1.20  2004/09/20 20:31:11  venglisc
-# Added CdrDocumentation.py menu item for PDF formatted CDR documentation.
-# (Bug 1338).
-#
-# Revision 1.19  2003/02/13 23:03:01  pzhang
-# Added Publishling item.
-#
-# Revision 1.18  2002/05/24 20:40:38  bkline
-# Fixed misnamed variable (request changed to action).
-#
-# Revision 1.17  2002/05/24 20:37:31  bkline
-# New Report Menu structure implemented.
-#
-# Revision 1.16  2002/05/24 18:02:38  bkline
-# Added Person Protocol Review report.
-#
-# Revision 1.15  2002/05/16 14:34:42  bkline
-# Added future entry for newly published trials.
-#
-# Revision 1.14  2002/05/03 20:28:54  bkline
-# New Mailer reports.
-#
-# Revision 1.13  2002/04/25 02:58:53  bkline
-# New report for mailer checkin.
-#
-# Revision 1.12  2002/04/22 22:15:45  bkline
-# New report (Generic Date Last Modified).
-#
-# Revision 1.11  2002/04/10 20:07:55  bkline
-# Added Publishing Job Queue report.
-#
-# Revision 1.10  2002/03/21 20:01:15  bkline
-# Added Glossary Term Links report and Persons at Org report.
-#
-# Revision 1.9  2002/03/14 04:01:10  bkline
-# Added New Doc Report.
-#
-# Revision 1.8  2002/03/13 16:58:25  bkline
-# Added dated actions report.
-#
-# Revision 1.7  2002/03/09 03:27:40  bkline
-# Added report for unverified citations.
-#
-# Revision 1.6  2002/03/02 13:50:31  bkline
-# Added report for checked-out documents.
-#
-# Revision 1.5  2002/02/26 18:55:29  bkline
-# Sorted report menu alphabetically.
-#
-# Revision 1.4  2002/02/21 15:22:03  bkline
-# Added navigation buttons.
-#
-# Revision 1.3  2002/01/22 21:32:01  bkline
-# Added three new reports.
-#
-# Revision 1.2  2001/12/01 18:07:34  bkline
-# Added some new reports.
-#
-# Revision 1.1  2001/06/13 22:16:32  bkline
-# Initial revision
+# Reports submenu for CDR administrative system.
 #
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, string
@@ -133,7 +54,7 @@ reports = [
           ]
 
 for r in reports:
-    form += "<LI><A HREF='%s/%s?%s=%s'>%s</LI>\n" % (
+    form += "<LI><A HREF='%s/%s?%s=%s'>%s</A></LI>\n" % (
             cdrcgi.BASE, r[0], cdrcgi.SESSION, session, r[1])
 
 cdrcgi.sendPage(header + form + "</OL></FORM></BODY></HTML>")
