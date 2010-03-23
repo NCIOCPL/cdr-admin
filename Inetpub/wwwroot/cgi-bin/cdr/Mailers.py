0105722db4ada862d6dc1b4c7d1bf2bc5836bdbb
Bob Kline <bkline@rksystems.com>
2010-03-23 17:58:08 +0000

    <R9535> Added GP mailers

--- Inetpub/wwwroot/cgi-bin/cdr/Mailers.py
+++ Inetpub/wwwroot/cgi-bin/cdr/Mailers.py
@@ -4,37 +4,7 @@
 #
 # Main menu for mailer jobs.
 #
-# $Log: not supported by cvs2svn $
-# Revision 1.10  2004/07/17 11:52:54  bkline
-# Plugged in user interface for generating PDQ Board Member Correspondence
-# Mailers.
-#
-# Revision 1.9  2004/07/13 17:47:06  bkline
-# Added electronic mailer review.
-#
-# Revision 1.8  2002/10/24 20:10:42  bkline
-# Added separate items for Editorial and Advisory boards mailers.
-#
-# Revision 1.7  2002/10/24 02:50:38  bkline
-# Fixed typo (missing closing parenthesis).
-#
-# Revision 1.6  2002/10/24 02:49:45  bkline
-# Further consolidation of menu items.
-#
-# Revision 1.5  2002/06/04 19:15:01  ameyer
-# Removed New Physician Initial Mailer - now part of directory mailers.
-#
-# Revision 1.4  2002/03/19 23:46:36  ameyer
-# Added line for directory mailers.  Will eventually remove physician mailer.
-#
-# Revision 1.3  2002/02/21 22:34:00  bkline
-# Added navigation buttons.
-#
-# Revision 1.2  2001/12/25 01:19:06  bkline
-# Added missing </A> tag; added two mailers.
-#
-# Revision 1.1  2001/12/01 18:11:44  bkline
-# Initial revision
+# BZIssue::4630
 #
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, string
@@ -61,6 +31,7 @@ if action == cdrcgi.MAINMENU:
 #----------------------------------------------------------------------
 form = "<OL>\n"
 reports = (('DirectoryMailerReqForm.py?', 'Directory Mailers'),
+           ('GPMailerReqForm.py?',  'Genetics Professional Mailers'),
            ('ProtocolMailerReqForm.py?',  'Protocol Mailers'),
            ('SummaryMailerReqForm.py?BoardType=Editorial&',
             'Summary Mailers (Editorial Board)'),
