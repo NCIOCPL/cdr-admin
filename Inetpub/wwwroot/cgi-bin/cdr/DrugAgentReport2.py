0ee37fba4e95cd073c21af37c47c7471f6bff51e
Bob Kline <bkline@rksystems.com>
2010-03-23 14:29:56 +0000

    <R9531> Dramatically improved efficiency and switched to ExcelWriter module

--- Inetpub/wwwroot/cgi-bin/cdr/DrugAgentReport2.py
+++ Inetpub/wwwroot/cgi-bin/cdr/DrugAgentReport2.py
@@ -4,15 +4,13 @@
 #
 # Request #1602 (second report on Drug/Agent terms).
 #
-# $Log: not supported by cvs2svn $
-# Revision 1.2  2005/05/25 19:00:05  bkline
-# Added new columns requested by Sheri (request #1695).
-#
-# Revision 1.1  2005/03/24 21:15:04  bkline
-# Drug/Agent Other Names Report.
+# BZIssue::1602
+# BZIssue::1695
+# BZIssue::2337
+# BZIssue::4784
 #
 #----------------------------------------------------------------------
-import cdrdb, cgi, pyXLWriter, sys, time
+import cdrdb, cgi, ExcelWriter, sys, time, lxml.etree as etree
 
 # The report can now produce drug information terms filtered
 #   by protocol status as before, or unfiltered if requested.
@@ -30,40 +28,36 @@ class Term:
         self.id         = id
         self.name       = u""
         self.otherNames = []
-        cursor.execute("""\
-            SELECT value
-              FROM query_term
-             WHERE path = '/Term/PreferredName'
-               AND doc_id = ?""", id)
-        rows = cursor.fetchall()
-        if rows:
-            self.name = rows[0][0]
-        cursor.execute("""\
-   SELECT DISTINCT n.value, t.value, v.value, r.value, s.value
-              FROM query_term n
-              JOIN query_term t
-                ON n.doc_id = t.doc_id
-               AND LEFT(n.node_loc, 4) = LEFT(t.node_loc, 4)
-   LEFT OUTER JOIN query_term v
-                ON v.doc_id = t.doc_id
-               AND LEFT(v.node_loc, 4) = LEFT(t.node_loc, 4)
-               AND v.path = '/Term/OtherName/SourceInformation'
-                          + '/VocabularySource/SourceCode'
-   LEFT OUTER JOIN query_term r
-                ON r.doc_id = t.doc_id
-               AND LEFT(r.node_loc, 4) = LEFT(t.node_loc, 4)
-               AND r.path = '/Term/OtherName/SourceInformation/ReferenceSource'
-   LEFT OUTER JOIN query_term s
-                ON s.doc_id = t.doc_id
-               AND LEFT(s.node_loc, 12) = LEFT(v.node_loc, 12)
-               AND s.path = '/Term/OtherName/SourceInformation'
-                          + '/VocabularySource/SourceTermId'
-             WHERE n.path = '/Term/OtherName/OtherTermName'
-               AND t.path = '/Term/OtherName/OtherNameType'
-               AND n.doc_id = ?""", id)
-        for row in cursor.fetchall():
-            self.otherNames.append(row)
-        self.otherNames.sort(lambda a,b: cmp(a[0].upper(), b[0].upper()))
+        cursor.execute("SELECT xml FROM document WHERE id = ?", id)
+        tree = etree.XML(cursor.fetchone()[0].encode('utf-8'))
+        for e in tree.findall('PreferredName'):
+            self.name = e.text
+        for e in tree.findall('OtherName'):
+            self.otherNames.append(Term.OtherName(e))
+        self.otherNames.sort()
+    def __cmp__(self, other):
+        return cmp(self.name, other.name)
+    class OtherName:
+        def __init__(self, node):
+            self.name = self.nameType = self.vocabularyCode = None
+            self.referenceSource = self.sourceTermId = None
+            for child in node:
+                if child.tag == 'OtherTermName':
+                    self.name = child.text
+                    self.nameUpper = self.name.upper()
+                elif child.tag == 'OtherNameType':
+                    self.nameType = child.text
+                elif child.tag == 'SourceInformation':
+                    for grandchild in child.findall('ReferenceSource'):
+                        self.referenceSource = grandchild.text
+                    for grandchild in child.findall('VocabularySource'
+                                                    '/SourceCode'):
+                        self.vocabularyCode = grandchild.text
+                    for grandchild in child.findall('VocabularySource'
+                                                    '/SourceTermId'):
+                        self.sourceTermId = grandchild.text
+        def __cmp__(self, other):
+            return cmp(self.nameUpper, other.nameUpper)
 
 conn = cdrdb.connect('CdrGuest')
 cursor = conn.cursor()
@@ -113,57 +107,58 @@ else:
               ORDER BY u.int_val""")
 
 terms = []
+#start = time.time()
 for row in cursor.fetchall():
     terms.append(Term(row[0]))
+#delta = time.time() - start
+#print "%f seconds to process 100 docs" % delta
+#sys.exit(0)
 t = time.strftime("%Y%m%d%H%M%S")
-terms.sort(lambda a,b: cmp(a.name, b.name))
+terms.sort()
 print "Content-type: application/vnd.ms-excel"
 print "Content-Disposition: attachment; filename=DrugAgentReport-%s.xls" % t
 print
 
-workbook = pyXLWriter.Writer(sys.stdout)
-worksheet = workbook.add_worksheet("Terms")
+book = ExcelWriter.Workbook()
+sheet = book.addWorksheet("Terms")
+font = ExcelWriter.Font('white', bold=True)
+alignment = ExcelWriter.Alignment('Center')
+interior = ExcelWriter.Interior('blue')
+format = book.addStyle(font=font, interior=interior, alignment=alignment)
+sheet.addCol(1, 38)
+sheet.addCol(2, 273)
+sheet.addCol(3, 273)
+sheet.addCol(4, 137)
+sheet.addCol(5, 164)
+sheet.addCol(6, 44)
+row = sheet.addRow(1, format)
+row.addCell(1, "CDR ID")
+row.addCell(2, "Preferred Name")
+row.addCell(3, "Other Names")
+row.addCell(4, "Other Name Type")
+row.addCell(5, "Source")
+row.addCell(6, "SourceID")
+rowNum = 2
 
-format = workbook.add_format()
-format.set_bold();
-format.set_color('white')
-format.set_bg_color('blue')
-format.set_align('center')
-
-worksheet.set_column(0, 7)
-worksheet.set_column(1, 50)
-worksheet.set_column(2, 50)
-worksheet.set_column(3, 25)
-worksheet.set_column(4, 30)
-worksheet.set_column(5, 8)
-worksheet.write([0, 0], "CDR ID", format)
-worksheet.write([0, 1], "Preferred Name", format)
-worksheet.write([0, 2], "Other Names", format)
-worksheet.write([0, 3], "Other Name Type", format)
-worksheet.write([0, 4], "Source", format)
-worksheet.write([0, 5], "SourceID", format)
-row = 1
-
-def fix(name):
-    return (name.replace(u'\u2120', u'(SM)')
-                .replace(u'\u2122', u'(TM)')
-                .encode('latin-1', 'ignore'))
 for term in terms:
-    worksheet.write([row, 0], term.id)
-    worksheet.write_string([row, 1], fix(term.name))
-    for i in range(len(term.otherNames)):
-        name = fix(term.otherNames[i][0])
-        nameType = fix(term.otherNames[i][1])
-        worksheet.write_string([row, 2], name)
-        worksheet.write_string([row, 3], nameType)
-        if term.otherNames[i][2]:
-            worksheet.write_string([row, 4], fix(term.otherNames[i][2] +
-                                                 " Vocabulary"))
-        elif term.otherNames[i][3]:
-            worksheet.write_string([row, 4], fix(term.otherNames[i][3]))
-        if term.otherNames[i][4]:
-            worksheet.write_string([row, 5], fix(term.otherNames[i][4]))
-        row += 1
+    row = sheet.addRow(rowNum)
+    row.addCell(1, term.id)
+    row.addCell(2, term.name)
+    first = True
+    for otherName in term.otherNames:
+        if first:
+            first = False
+        else:
+            row = sheet.addRow(rowNum)
+        row.addCell(3, otherName.name)
+        row.addCell(4, otherName.nameType)
+        if otherName.vocabularyCode:
+            row.addCell(5, u"%s Vocabulary" % otherName.vocabularyCode)
+        else:
+            row.addCell(5, otherName.referenceSource)
+        if otherName.sourceTermId:
+            row.addCell(6, otherName.sourceTermId)
+        rowNum += 1
     if not term.otherNames:
-        row += 1
-workbook.close()
+        rowNum += 1
+book.write(sys.stdout, True)
