c831850b3c2edef4c4e4a90d89706676aeac44ca
Bob Kline <bkline@rksystems.com>
2010-05-11 20:04:31 +0000

    <R9610> Added CTGovProtocol Processing Status report

--- Inetpub/wwwroot/cgi-bin/cdr/CTGov.py
+++ Inetpub/wwwroot/cgi-bin/cdr/CTGov.py
@@ -5,51 +5,7 @@
 # Submenu for ClinicalTrials.gov activities.
 #
 # BZIssue::4700
-#
-# Revision 1.14  2007/11/06 23:07:05  ameyer
-# Added menu entry for GlobalChangeCTGovMapping.py.
-#
-# Revision 1.13  2006/11/29 16:16:08  bkline
-# Plugged in command to force import from CT.gov.
-#
-# Revision 1.12  2006/04/26 17:23:12  venglisc
-# Adding new menu item for Unpublished CTGovProtocol by Phase report.
-# Sorting the menu items alphabetically. (Bug 2048)
-#
-# Revision 1.11  2006/01/10 20:09:44  ameyer
-# Minor change to the wording associated with the CTGovUpdateReport.
-#
-# Revision 1.10  2005/12/23 02:20:25  ameyer
-# Added CTGovUpdateReport.
-#
-# Revision 1.9  2005/11/30 06:02:09  ameyer
-# Added CTGovEntryDate report to the menu.
-#
-# Revision 1.8  2004/10/26 19:56:33  venglisc
-# Corrected the anker link for the menu items.
-#
-# Revision 1.7  2004/07/13 15:40:12  venglisc
-# Modified menu item to "Mark/Remove Protocols as Duplicates".
-#
-# Revision 1.6  2004/05/11 17:30:43  bkline
-# Plugged in CTGovMarkDuplicate.py.
-#
-# Revision 1.5  2004/01/14 14:04:40  bkline
-# Plugged in new CTGov download report.
-#
-# Revision 1.4  2003/12/16 15:36:45  bkline
-# Replaced some of the stubs.
-#
-# Revision 1.3  2003/11/25 12:44:48  bkline
-# Plugged in QC report.
-#
-# Revision 1.2  2003/11/10 17:54:58  bkline
-# Split interface for reviewing new protocols into two, separating out
-# those waiting for CIPS feedback.  Plugged in reports for import
-# statistics and external map failures.
-#
-# Revision 1.1  2003/11/03 16:09:03  bkline
-# Menu of actions for ClinicalTrials.gov protocols.
+# BZIssue::4804
 #
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, string
@@ -147,16 +103,11 @@ form += """\
     <H3>Management Reports</H3>
     <OL>
 """
-#          ('CTGovDownloadReport.py', 'Download Statistics Report'),
-#          ('CTGovImportReport.py', 'Import Statistics Report'),
-#          ('ExternMapFailures.py', 'External Map Failures Report'),
-#          ('CTGovOutOfScope.py', 'Records Marked Out of Scope'),
-#          ('CTGovDupReport.py', 'Records Marked Duplicate'),
-#          ('CTGovEntryDate.py', 'CTGovProtocols vs. Early EntryDate'),
-#          ('CTGovUpdateReport.py', 'Imported CTGovProtocols vs. CWDs'),
 
 reports = [
            ('CTGovUpdateReport.py', 'CTGovProtocols Imported vs. CWDs'),
+           ('CTGovProtocolProcessingStatusReport.py',
+            'CTGovProtocols Processing Status Report'),
            ('CTGovUnpublished.py', 'CTGovProtocols Unpublished with Phase'),
            ('CTGovEntryDate.py', 'CTGovProtocols vs. Early EntryDate'),
            ('ExternMapFailures.py', 'External Map Failures Report'),
