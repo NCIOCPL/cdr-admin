5f71b1aa5f0e3e11d6462a47839c6333c98afef3
Bob Kline <bkline@rksystems.com>
2010-10-12 13:43:11 +0000

    <R9920> Added comment documenting issues with code

--- Inetpub/wwwroot/cgi-bin/cdr/TermSearch.py
+++ Inetpub/wwwroot/cgi-bin/cdr/TermSearch.py
@@ -9,6 +9,91 @@
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, cdrdb, xml.dom.minidom, httplib, time, NCIThes
 
+ISSUES_WITH_THIS_CODE = """\
+Alan:
+
+I'm beginning to suspect that I should have scheduled more code
+walkthroughs for projects we assigned to [name redacted].  As part of
+my work on issue #4916 (failure of the thesaurus import command) I was
+examining his implementation of the Term update code and it took me a
+good while to figure out what he was doing (not a comment in sight).
+From what I can figure out, it looks as if he's doing something like
+the following:
+
+  set a global retval JavaScript variable to true
+  if the user clicks the Import button:
+    create an XML HTTP Request (AJAX) object
+    assign an asynchronous callback function for the object
+    use the object to send a synchronous request to the same Python script
+    return the value of retval to the handler for the submit (Import) button
+
+  if the callback function is invoked:
+    if the AJAX response is longer than 10 characters:
+      display the contents of the response in a dialog box
+      set the global retval JavaScript variable to false
+
+  in the Python script, if this is an invocation by the AJAX request:
+    retrieve the concept document for the user-specified concept code
+    extract the preferred name from the document
+    retrieve the CDR document for the user-specified CDR ID
+    extract the preferred name from that document
+    if the normalized names do not match:
+      return a string describing the mismatch
+    otherwise:
+      return an empty string
+  otherwise, if the user has requested an import:
+    retrieve the concept document for the user-specified concept code
+    import it
+
+Ignoring minor sillinesses (like checking for 11 or more characters in
+the AJAX response when his own code ensures that the response will
+contain either an empty string or an error message, or why he thought
+it was better to use lstrip().rstrip() instead of just strip() in a
+number of places), I can't see the benefit of using AJAX at all,
+unless he was deliberately trying to obfuscate the code, since it
+results in retrieving the same concept twice from the thesaurus
+service (and retrieving the same CDR term document twice).  And
+relying on the asynchronous callback to change the value of a global
+variable so that the return value from the callAjax() function will
+cause the handling of the Import button to abort processing makes my
+skin crawl.
+
+So my question for you is: should we leave this code alone (since it
+actually works, at least as far as I can tell - I'm not 100% certain
+that the asynchronous callback function will always be invoked if he's
+submitting the AJAX request synchronously) or is it so outrageous that
+we should take the trouble to rewrite it?
+
+-- 
+Bob Kline
+http://www.rksystems.com
+mailto:***REMOVED***
+
+If it works and the performance is acceptable for current and
+anticipated uses, my inclination would be to leave it alone but add
+comments explaining 1) how it works and 2) what needs to be done if it
+is refactored.
+
+A good and easy way to do that might just be to add your email message
+above as a long comment in the code.  That way the analysis you did
+will be kept right with the code - if we ever need it.  For example:
+
+ISSUES_WITH_THIS_CODE = '''
+<email here>
+'''
+
+Alternatively, we could add a low priority task for it if you think
+it's justified, though I'd guess that the users would want us doing
+other things as long as we didn't think it was going to cause
+problems.  Let's discuss it tomorrow.
+
+-- 
+Alan Meyer
+AM Systems, Inc.
+Randallstown, MD USA
+***REMOVED***
+"""
+
 #----------------------------------------------------------------------
 # Get the form variables.
 #----------------------------------------------------------------------
