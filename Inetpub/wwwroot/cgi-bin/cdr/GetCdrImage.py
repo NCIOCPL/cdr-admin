f615dfa7f816d53a0087ed6616b5bc0556c31fd2
Bob Kline <bkline@rksystems.com>
2011-03-08 19:43:29 +0000

    <R10044> Added support for Visuals Online (VOL): BZIssue::5001

--- Inetpub/wwwroot/cgi-bin/cdr/GetCdrImage.py
+++ Inetpub/wwwroot/cgi-bin/cdr/GetCdrImage.py
@@ -4,23 +4,50 @@
 #
 # Send JPEG version of a CDR image to the browser, possibly resized.
 #
-# $Log: not supported by cvs2svn $
-# Revision 1.1  2004/11/17 14:39:55  bkline
-# New scripts to support images in the CDR.
+# BZIssue::5001
 #
 #----------------------------------------------------------------------
 import cdrdb, sys, cgi, msvcrt, os, cdr, cdrcgi, Image, cStringIO
+import ImageEnhance
 
 msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
 message = "GetCdrImage: No doc ID found"
 fields  = cgi.FieldStorage()
-width   = fields and fields.getvalue("width")   or None
-fname   = fields and fields.getvalue("fname")   or None
-quality = fields and fields.getvalue("quality") or "75"
-cdrId   = fields and fields.getvalue("id")      or cdrcgi.bail(message)
+width   = fields.getvalue("width")
+res     = fields.getvalue("res")
+fname   = fields.getvalue("fname")
+quality = fields.getvalue("quality") or "85"
+sharpen = fields.getvalue("sharpen")
+cdrId   = fields.getvalue("id")      or cdrcgi.bail(message)
 
 conn    = cdrdb.connect()
 cursor  = conn.cursor()
+
+#----------------------------------------------------------------------
+# Support for Visuals Online.  Four possible values for res:
+#   300   - full size (for printing)
+#    72   - 24% (viewing on screen)
+#   150   - 1/2 size (in between)
+#   thumb - max 120px on a side (thumbnails)
+#----------------------------------------------------------------------
+def widthFromRes(size, res):
+    width, height = size
+    if res == "300":
+        return None # don't resize
+    elif res == "150":
+        return int(round(width / 2.0))
+    elif res == "72":
+        return int(round(width * .24))
+    elif res == "thumb":
+        if width >= height:
+            if width <= 120:
+                return None
+            return 120
+        if height <= 120:
+            return None
+        return int(round(width * (120.0 / height)))
+    else:
+        cdrcgi.bail("invalid res value '%s'" % res)
 
 # If the docId comes in in the format 'CDR99999-111.jpg' it is coming 
 # from the PublishPreview with a size postfix.
@@ -50,7 +77,9 @@ if image.mode == 'P':
 try:
     quality = int(quality)
 except:
-    quality = 75
+    quality = 85
+if res:
+    width = widthFromRes(image.size, res)
 if width:
     try:
         width = int(width)
@@ -62,7 +91,14 @@ if width:
     except Exception, e:
         cdrcgi.bail("Failure resizing %s: %s" % (docId, str(e)))
 newImageFile = cStringIO.StringIO()
-image.save(newImageFile, "JPEG", quality = 85)
+if sharpen:
+    try:
+        sharpen = float(sharpen)
+        enh = ImageEnhance.Sharpness(image)
+        image = enh.enhance(sharpen)
+    except:
+        pass
+image.save(newImageFile, "JPEG", quality=quality)
 bytes = newImageFile.getvalue()
 if fname:
     f = open(fname, "wb")
