da3cfd3d66a8f19c20113fc37286ed59f5a6aeef
Bob Kline <bkline@rksystems.com>
2012-01-12 19:53:27 +0000

    <R10282> New mailer (BZIssue::5162)

--- Inetpub/wwwroot/cgi-bin/cdr/BoardMemberMailerReqForm.py
+++ Inetpub/wwwroot/cgi-bin/cdr/BoardMemberMailerReqForm.py
@@ -372,6 +372,8 @@ header = cdrcgi.header(title, title, section, script, buttons,
    var letters = {
        'advisory': [
            new Option('Advisory Board Invitation Letter', 'adv-invitation'),
+           new Option('Advisory Board Still Interested Letter',
+                      'adv-interested'),
            new Option('Advisory Board Thank You Letter',  'adv-thankyou')
        ],
        'editorial': [
