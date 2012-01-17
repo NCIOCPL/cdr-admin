35e3601417e3f3e2ee1f74d26b265b16c912b305
Bob Kline <bkline@rksystems.com>
2012-01-17 14:45:48 +0000

    <R10294> Added CTRP mapping gaps tool

--- Inetpub/wwwroot/cgi-bin/cdr/CiatCipsStaff.py
+++ Inetpub/wwwroot/cgi-bin/cdr/CiatCipsStaff.py
@@ -4,38 +4,11 @@
 #
 # Main menu for CIAT/CIPS staff.
 #
+# BZIssue::1365
+# BZIssue::4009
 # BZIssue::4700
 # BZIssue::5013 - [Glossary Audio] Create Audio Download Tool
-#
-# Revision 1.10  2009/07/22 01:25:13  ameyer
-# Added GlobalChangeSimpleLink.py.
-#
-# Revision 1.9  2009/04/28 17:57:26  ameyer
-# Renamed Global Changes label for clearer explanation of what it does.
-#
-# Revision 1.8  2008/09/26 00:19:06  ameyer
-# Added functions to replace doc with new doc, replace CWD with older ver.
-#
-# Revision 1.7  2008/04/17 18:55:40  bkline
-# Re-arrangement of CIAT menus at Sheri's request (#4009).
-#
-# Revision 1.6  2007/10/22 16:02:15  bkline
-# Added Oncore report.
-#
-# Revision 1.5  2007/06/14 20:08:54  bkline
-# Plugged in command to upload GENETICSPROFESSIONAL documents.
-#
-# Revision 1.4  2006/11/29 16:18:43  bkline
-# Added new command for reviewing new CTS trials.
-#
-# Revision 1.3  2004/11/01 21:27:00  venglisc
-# Added menu item to ftp images from CIPSFTP.  Alphabetized list. (Bug 1365)
-#
-# Revision 1.2  2004/08/10 15:39:26  bkline
-# Plugged in new menu items for editing the external mapping values.
-#
-# Revision 1.1  2003/12/16 16:06:08  bkline
-# Main menu for CIAT/CIPS staff.
+# BZIssue::4942 - CTRP mapping gaps
 #
 #----------------------------------------------------------------------
 import cgi, cdr, cdrcgi, re, string
@@ -65,6 +38,7 @@ items   = (('AdvancedSearch.py', 'Advanced Search',       ''               ),
            ('GlossaryTermAudioReview.py', 'Audio Review Glossary Term',  ''),
            ('getBatchStatus.py', 'Batch Job Status',      ''               ),
            ('CTGov.py',          'CTGov Protocols',       ''               ),
+           ('ctrp-mapping-gaps.py', 'CTRP Mapping Gaps',  ''               ),
            ('GlobalChange.py',   'Global Change Protocol Links', ''        ),
            ('GlobalChangeZipCode.py', 'Global Change Zip Codes', ''        ),
            ('FtpImages.py',      'Images Download',       ''               ),
