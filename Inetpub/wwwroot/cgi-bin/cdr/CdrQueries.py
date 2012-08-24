c2597860d6f4a0285529c0a4a3f1e687bd918205
Bob Kline <bkline@rksystems.com>
2012-08-24 20:51:07 +0000

    <R10518> Housekeeping cleanup of CGI scripts

--- Inetpub/wwwroot/cgi-bin/cdr/CdrQueries.py
+++ Inetpub/wwwroot/cgi-bin/cdr/CdrQueries.py
@@ -5,13 +5,6 @@
 #
 # Ad-hoc SQL query tool for CDR database.
 #
-# $Log: not supported by cvs2svn $
-# Revision 1.2  2003/11/05 14:47:14  bkline
-# Increased timeout to 5 minutes.
-#
-# Revision 1.1  2002/12/10 13:34:06  bkline
-# Add-hoc SQL query tool for CDR database.
-#
 #----------------------------------------------------------------------
 
 import CgiQuery, cdrdb
