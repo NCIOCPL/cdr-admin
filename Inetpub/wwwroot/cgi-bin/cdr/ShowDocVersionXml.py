2ccee1d48174fd9f18b6bc385709d763613d528c
Bob Kline <bkline@rksystems.com>
2012-08-28 15:45:44 +0000

    <R10578> Tool to retrieve raw XML for specific version of CDR document

---
+++ Inetpub/wwwroot/cgi-bin/cdr/ShowDocVersionXml.py
@@ -0,0 +1,32 @@
+#----------------------------------------------------------------------
+#
+# $Id$
+#
+#----------------------------------------------------------------------
+import cgi, cdrdb, cdrcgi
+
+#----------------------------------------------------------------------
+# Get the parameters from the request.
+#----------------------------------------------------------------------
+fields  = cgi.FieldStorage()
+docId   = fields.getvalue('id')
+ver     = fields.getvalue('ver')
+
+#----------------------------------------------------------------------
+# Get the document version.
+#----------------------------------------------------------------------
+cursor = cdrdb.connect('CdrGuest').cursor()
+cursor.execute("""\
+SELECT xml
+  FROM doc_version
+ WHERE id = ?
+   AND num = ?""", (docId, ver))
+rows = cursor.fetchall()
+if not rows:
+    cdrcgi.bail("Version %s of document %s not found" % (ver, docId))
+
+#----------------------------------------------------------------------
+# Send it.
+#----------------------------------------------------------------------
+docXml = rows[0][0]
+print "Content-type: text/xml; charset=utf-8\n\n" + docXml.encode('utf-8')
