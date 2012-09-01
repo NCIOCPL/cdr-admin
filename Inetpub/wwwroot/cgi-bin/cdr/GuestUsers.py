641c6bd99b78de4c343404bf3c819c868ff3be69
Bob Kline <bkline@rksystems.com>
2012-09-01 15:31:40 +0000

    <R10597> Saving Volker's changes before modifying to make usable on all three servers

--- Inetpub/wwwroot/cgi-bin/cdr/GuestUsers.py
+++ Inetpub/wwwroot/cgi-bin/cdr/GuestUsers.py
@@ -31,15 +31,63 @@ session = "?%s=%s" % (cdrcgi.SESSION, session)
 title   = "CDR Administration"
 section = "Guest Users"
 buttons = []
-html    = cdrcgi.header(title, title, section, "", buttons) + """\
+html    = cdrcgi.header(title, title, section, "", buttons,
+                        stylesheet = """
+        <style type='text/css'>
+        </style>
+            
+        <script language='JavaScript'>
+          function setCdrId() {
+             var link = document.getElementById('pp2link');
+             var docid = document.getElementById('cdrid').value;
+             var pp2link = link + docid;
+             // alert(pp2link);
+             document.getElementById('pp2link').href = pp2link
+             }
+
+          function setDocType() {
+             var link = document.getElementById('pplink');
+             var index = document.getElementById('ppdt').selectedIndex;
+             var doctype = document.getElementById('ppdt').options[index].text;
+             var pplink = link + doctype;
+             //alert(document.getElementById('ppdt').selectedIndex);
+             //alert(document.getElementById('ppdt').value);
+             //alert(document.getElementById('ppdt').options[index].text);
+             //alert(pplink);
+             document.getElementById('pplink').href = pplink
+             }
+        </script>
+        """) + """\
    <ol>
 """
 items   = (('AdvancedSearch.py',       'Advanced Search Menu'          ),
+           ('PublishPreview.py',       'Publish Preview'),
            ('TerminologyReports.py',   'Terminology Reports'),
            ('Logout.py',               'Log Out'                       )
            )
 for item in items:
-    html += """\
+    if item[1] == 'Publish Preview2':
+        html += """\
+    <li><a href='%s/%s%s&ReportType=pp&DocType=' 
+           id="pplink" onclick=setDocType()>%s</a> &nbsp;
+    <select name="pptype" size="1" id="ppdt">
+     <option value="1">CTGovProtocol</option>
+     <option value="2">DrugInformationSummary</option>
+     <option value="3">GlossaryTermName</option>
+     <option value="4">InScopeProtocol</option>
+     <option value="5">Summary</option>
+    </select></li>
+""" % (cdrcgi.BASE, item[0], session, item[1])
+    elif item[1] == 'Publish Preview':
+        html += """\
+    <li><a href='%s/%s%s&ReportType=pp&DocId=' 
+           id="pp2link" onclick=setCdrId()>%s</a> &nbsp;
+    &nbsp;
+    <span style="font-weight: normal;">(Enter CDR-ID)</span>&nbsp;
+    <input name="ppid"  id="cdrid"></li>
+""" % (cdrcgi.BASE, item[0], session, item[1])
+    else:
+        html += """\
     <li><a href='%s/%s%s'>%s</a></li>
 """ % (cdrcgi.BASE, item[0], session, item[1])
 
