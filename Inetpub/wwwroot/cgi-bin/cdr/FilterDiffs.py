ca58d252b766e06c6acbff4876e02fd078a3f6a0
Bob Kline <bkline@rksystems.com>
2013-08-15 20:26:38 +0000

    <R11957> Made the FilterDiffs report work in the CBIIT environment (OCECDR-3623)

--- Inetpub/wwwroot/cgi-bin/cdr/FilterDiffs.py
+++ Inetpub/wwwroot/cgi-bin/cdr/FilterDiffs.py
@@ -2,49 +2,27 @@
 #
 # $Id$
 #
-# Compare filters between two servers.
+# Compare filters between the current tier and the production tier.
+#
+# OCECDR-3623
 #
 #----------------------------------------------------------------------
-
-import cdr, cdrdb, cgi, cdrcgi, os, tempfile, re
-
-#----------------------------------------------------------------------
-# Get the form variables.
-#----------------------------------------------------------------------
-fields   = cgi.FieldStorage()
-server1  = fields and fields.getvalue('s1')
-server2  = fields and fields.getvalue('s2')
-pattern  = re.compile("diff -aur %s/@@MARK@@(.*?)@@MARK@@" % server1)
-
-if not server1 or not server2:
-    cdrcgi.bail("Must specify parameters s1 and s2")
-
+import cdr, cdrdb, cgi, cdrcgi, os, tempfile, re, shutil, glob
 
 #----------------------------------------------------------------------
-# Object for results of an external command.
+# Make sure we're not on the production server.
 #----------------------------------------------------------------------
-class CommandResult:
-    def __init__(self, code, output):
-        self.code   = code
-        self.output = output
-
-#----------------------------------------------------------------------
-# Run an external command.
-#----------------------------------------------------------------------
-def runCommand(command):
-    commandStream = os.popen('%s 2>&1' % command)
-    output = commandStream.read()
-    code = commandStream.close()
-    return CommandResult(code, output)
+if cdr.isProdHost():
+    cdrcgi.bail("Can't compare the production server to itself")
 
 #----------------------------------------------------------------------
 # Create a temporary working area.
 #----------------------------------------------------------------------
 def makeTempDir():
-    if os.environ.has_key("TMP"):
-        tempfile.tempdir = os.environ["TMP"]
+    tempfile.tempdir = "d:\\tmp"
     where = tempfile.mktemp("diff")
     abspath = os.path.abspath(where)
+    print abspath
     try: os.mkdir(abspath)
     except: cdrcgi.bail("Cannot create directory %s" % abspath)
     try: os.chdir(abspath)
@@ -54,15 +32,15 @@ def makeTempDir():
     return abspath
 
 #----------------------------------------------------------------------
-# Get the filters from one of the two servers.
+# Get the filters from the local tier.
 #----------------------------------------------------------------------
-def getFilters(tmpDir, server):
-    try: os.mkdir(server)
+def getLocalFilters(tmpDir):
+    try: os.mkdir(cdr.h.tier)
     except: 
         cleanup(tmpDir)
-        cdrcgi.bail("Cannot create directory %s" % server)
+        cdrcgi.bail("Cannot create directory %s" % cdr.h.tier)
     try:
-        conn = cdrdb.connect('CdrGuest', dataSource = server)
+        conn = cdrdb.connect('CdrGuest')
         curs = conn.cursor()
         curs.execute("""\
             SELECT d.title, d.xml
@@ -73,7 +51,7 @@ def getFilters(tmpDir, server):
         rows = curs.fetchall()
     except Exception, e:
         cleanup(tmpDir)
-        cdrcgi.bail('Database failure on %s: %s' % (server, e))
+        cdrcgi.bail('Database failure: %s' % e)
     for row in rows:
         try:
             title = row[0].replace(" ", "@@SPACE@@") \
@@ -81,11 +59,28 @@ def getFilters(tmpDir, server):
                           .replace("/", "@@SLASH@@") \
                           .replace("*", "@@STAR@@")
             xml = row[1].replace("\r", "")
-            filename = "%s/@@MARK@@%s@@MARK@@" % (server, title)
+            filename = "%s/@@MARK@@%s@@MARK@@" % (cdr.h.tier, title)
             open(filename, "w").write(xml.encode('utf-8'))
         except:
             cleanup(tmpDir)
             cdrcgi.bail("Failure writing %s" % filename);
+
+#----------------------------------------------------------------------
+# Make a copy of the filters on the production server.
+#----------------------------------------------------------------------
+def getProdFilters(tmpDir):
+    try:
+        os.mkdir("PROD")
+    except:
+        cleanup(tmpDir)
+        cdrcgi.bail("Cannot create directory PROD")
+    for oldpath in glob.glob("d:/cdr/prod-filters/*"):
+        try:
+            newpath = "PROD/%s" % os.path.basename(oldpath)
+            shutil.copy(oldpath, newpath)
+        except:
+            cleanup(tmpDir)
+            cdrcgi.bail("Failure writing %s" % newpath)
 
 #----------------------------------------------------------------------
 # Don't leave dross around if we can help it.
@@ -93,7 +88,7 @@ def getFilters(tmpDir, server):
 def cleanup(abspath):
     try:
         os.chdir("..")
-        runCommand("rm -rf %s" % abspath)
+        cdr.runCommand("rm -rf %s" % abspath)
     except:
         pass
 
@@ -112,27 +107,22 @@ def unEncode(str):
 #----------------------------------------------------------------------
 def makeBanner(name):
     line = "*" * 79 + "\n"
-    leftOver = 77 - len(name)
-    if leftOver > 0:
-        leftHalf = leftOver / 2
-        rightHalf = leftOver - leftHalf
-        middleLine = "%s %s %s\n" % ("*" * leftHalf, name, "*" * rightHalf)
-    else:
-        middleLine = name + "\n"
-    return "\n\n%s%s%s\n" % (line * 2, middleLine, line * 2)
+    name = (" %s " % name).center(79, "*")
+    return "\n\n%s%s\n%s\n" % (line * 2, name, line * 2)
 
 #----------------------------------------------------------------------
 # Get the filters.
 #----------------------------------------------------------------------
 workDir = makeTempDir()
-getFilters(workDir, server1)
-getFilters(workDir, server2)
+getLocalFilters(workDir)
+getProdFilters(workDir)
 
 #----------------------------------------------------------------------
 # Compare the filters.
 #----------------------------------------------------------------------
-result  = runCommand("diff -aur %s %s" % (server1, server2))
+result  = cdr.runCommand("diff -aur PROD %s" % cdr.h.tier)
 lines   = result.output.splitlines()
+pattern = re.compile("diff -aur PROD/@@MARK@@(.*?)@@MARK@@")
 for i in range(len(lines)):
     match = pattern.match(lines[i])
     if match:
@@ -149,7 +139,7 @@ Content-type: text/html; charset: utf-8
   <title>Filter Comparison Results</title>
  </head>
  <body>
-  <h3>The following filters differ between %s and %s</h3>
+  <h3>The following filters differ between PROD and %s</h3>
   <pre>%s</pre>
  </body>
-</html>""" % (server1, server2, report)
+</html>""" % (cdr.h.tier, report)
