5168c8d5f9d2d0c104024dbc09f983fb9e523e4a
Bob Kline <bkline@rksystems.com>
2017-11-26 09:58:25 -0500

    Adjust call to cdr.publish() (OCECDR-4211)

--- Inetpub/wwwroot/cgi-bin/cdr/publishing.py
+++ Inetpub/wwwroot/cgi-bin/cdr/publishing.py
@@ -317,10 +317,15 @@ class Control:
             email = self.fields.getvalue("email")
         else:
             email = "Do not notify"
-        response = cdr.publish(self.session, self.system.name,
-                               self.subset.name, parameters, doc_ids,
-                               email, no_output, port=cdr.getPubPort(),
-                               allowInActive=inactive_ok)
+        args = self.session, self.system.name, self.subset.name
+        opts = dict(
+            parms=parameters,
+            docList=doc_ids,
+            email=email,
+            noOutput=no_output,
+            allowInActive=inactive_ok
+        )
+        response = cdr.publish(*args, **opts)
         job_id, errors = response
         self.pageopts["subtitle"] = self.subset.name
         page = cdrcgi.Page(self.title, **self.pageopts)
