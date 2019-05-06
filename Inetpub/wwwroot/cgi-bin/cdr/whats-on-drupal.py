dfb893c6c2e4cc4af2c90af31629275eeb7fccc8
Bob Kline <bkline@rksystems.com>
2019-05-06 16:39:22 -0400

    Clean up code and add comments

--- Inetpub/wwwroot/cgi-bin/cdr/whats-on-drupal.py
+++ Inetpub/wwwroot/cgi-bin/cdr/whats-on-drupal.py
@@ -9,24 +9,41 @@ from cdrapi.publishing import DrupalClient
 from cdrapi.users import Session
 from cdrcgi import Control, Report
 
+
 class DrupalSummaries(Control):
+
     TITLE = "PDQ Summaries on Drupal"
+
     def __init__(self):
+        """
+        Collect report settings
+        """
+
         Control.__init__(self, self.PAGE_TITLE, self.TITLE)
         session = Session(self.session)
         host = session.tier.hosts.get("DRUPAL")
         self.host = self.fields.getvalue("host") or host
         self.base = "https://{}".format(self.host)
         self.client = DrupalClient(session, base=self.base)
+
     def populate_form(self, form):
+        """
+        Let the user choose another Drupal server than the tier's default
+        """
+
         form.add("<fieldset>")
         form.add(form.B.LEGEND("Drupal Server"))
         form.add_text_field("host", "Server", value=self.host)
         form.add("</fieldset>")
         form.add_output_options("html", None)
+
     def build_tables(self):
+        """
+        Populate a table with the CMS summaries the Drupal server says it has
+        """
+
         catalog = self.client.list()
-        headers = "CDR ID", "Title", "Type", "Language" #, "Created", "Changed"
+        headers = "CDR ID", "Title", "Type", "Language"
         cols = [Report.Column(header) for header in headers]
         rows = []
         for summary in catalog:
@@ -38,14 +55,18 @@ class DrupalSummaries(Control):
                 title,
                 summary_type,
                 language,
-                #str(summary.created),
-                #str(summary.changed),
             )
             rows.append(row)
         table = Report.Table(cols, rows)
         return [table]
+
     def lookup_title(self, cdr_id):
+        """
+        Get the display portion of the summary title
+        """
+
         doc = Doc(self.client.session, id=cdr_id)
         return doc.title.split(";")[0].strip()
+
 
 DrupalSummaries().run()
