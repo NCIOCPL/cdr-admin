509db2fb36b92f8f14517455c54c2586dad023b8
Bob Kline <bkline@rksystems.com>
2021-03-16 14:35:36 -0400

    Add separate citation QC report form (OCECDR-4956)

--- Inetpub/wwwroot/cgi-bin/cdr/CitationReports.py
+++ Inetpub/wwwroot/cgi-bin/cdr/CitationReports.py
@@ -14,13 +14,18 @@ class Control(Controller):
         """Add the menu items for Citations."""
         page.body.set("class", "admin-menu")
         page.form.append(page.B.H3("QC Reports"))
-        link = page.menu_link("CiteSearch.py", "Citation QC Report")
+        opts = dict(DocType="Citation")
+        link = page.menu_link("QcReport.py", "Citation QC Report", **opts)
         ol = page.B.OL(page.B.LI(link))
         page.form.append(ol)
         page.form.append(page.B.H3("Other Reports"))
-        link = page.menu_link("UnverifiedCitations.py", "Unverified Citations")
-        ol = page.B.OL(page.B.LI(link))
+        ol = page.B.OL()
         page.form.append(ol)
+        for display, script in (
+            ("Advanced Citation Search", "CiteSearch.py"),
+            ("Unverified Citations", "UnverifiedCitations.py"),
+        ):
+            ol.append(page.B.LI(page.menu_link(script, display)))
         page.form.append(page.B.H3("Management Reports"))
         ol = page.B.OL()
         page.form.append(ol)
