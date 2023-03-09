0066205ca02fc248343a4d4bdba5590c3ab6c3eb
Bob Kline <bkline@rksystems.com>
2023-03-09 08:43:16 -0500

    Add SVCP support to Summaries Lists report (OCECDR-5199)

--- Inetpub/wwwroot/cgi-bin/cdr/SummariesLists.py
+++ Inetpub/wwwroot/cgi-bin/cdr/SummariesLists.py
@@ -31,6 +31,12 @@ function check_board(val) {
     jQuery(".some").prop("checked", false);
   else
     jQuery("#board-all").prop("checked", false);
+}
+function check_included(val) {
+  if (val == "p")
+    jQuery("#select-summary-sets-box").hide();
+  else
+    jQuery("#select-summary-sets-box").show();
 }"""
 
     def build_tables(self):
@@ -64,16 +70,17 @@ function check_board(val) {
         opts = dict(label="Extra Cols", value="0")
         fieldset.append(page.text_field("extra", **opts))
         page.form.append(fieldset)
-        fieldset = page.fieldset("Select Summary Set(s)")
-        fieldset.append(page.checkbox("board", value="all", checked=True))
-        for id, name in sorted(self.boards.items(), key=itemgetter(1)):
-            opts = dict(value=id, label=name, classes="some")
-            fieldset.append(page.checkbox("board", **opts))
-        page.form.append(fieldset)
         fieldset = page.fieldset("Included Documents")
         for value, label, checked in self.INCLUDED:
             opts = dict(value=value, label=label, checked=checked)
             fieldset.append(page.radio_button("included", **opts))
+        page.form.append(fieldset)
+        fieldset = page.fieldset("Select Summary Set(s)")
+        fieldset.set("id", "select-summary-sets-box")
+        fieldset.append(page.checkbox("board", value="all", checked=True))
+        for id, name in sorted(self.boards.items(), key=itemgetter(1)):
+            opts = dict(value=id, label=name, classes="some")
+            fieldset.append(page.checkbox("board", **opts))
         page.form.append(fieldset)
         page.add_script(self.SCRIPT)
 
@@ -83,113 +90,106 @@ function check_board(val) {
         self.report.page.add_css(".report table { width: 1024px; }")
         self.report.send()
 
-    @property
+    @cached_property
     def audience(self):
         """Patient or Health Professional."""
 
-        if not hasattr(self, "_audience"):
-            default = self.AUDIENCES[0]
-            self._audience = self.fields.getvalue("audience", default)
-            if self._audience not in self.AUDIENCES:
-                self.bail()
-        return self._audience
+        default = self.AUDIENCES[0]
+        audience = self.fields.getvalue("audience", default)
+        if audience not in self.AUDIENCES:
+            self.bail()
+        return audience
 
-    @property
+    @cached_property
     def board(self):
         """Board(s) selected for the report."""
 
-        if not hasattr(self, "_board"):
-            ids = self.fields.getlist("board")
-            if not ids or "all" in ids:
-                self._board = [Board(self, id) for id in self.boards]
-            else:
-                self._board = []
-                for id in ids:
-                    if not id.isdigit():
-                        self.bail()
-                    id = int(id)
-                    if id not in self.boards:
-                        self.bail()
-                    self._board.append(Board(self, id))
-        return self._board
+        # OCECDR-5199: add support for SVPC summaries, which have no board.
+        if self.included == "p":
+            return [Board(self)]
 
-    @property
+        # Not SVPC-only: don't narrow by board if none are selected.
+        ids = self.fields.getlist("board")
+        if not ids or "all" in ids:
+            return [Board(self, id) for id in self.boards]# + [Board(self)]
+
+        # We've been asked to restrict the report to specific boards.
+        board = []
+        for id in ids:
+            if not id.isdigit():
+                self.bail()
+            id = int(id)
+            if id not in self.boards:
+                self.bail()
+            board.append(Board(self, id))
+        return board
+
+    @cached_property
     def boards(self):
-        """Boards available the report form's piclist."""
+        """Boards available on the report form's picklist."""
+        return self.get_boards()
 
-        if not hasattr(self, "_boards"):
-            self._boards = self.get_boards()
-        return self._boards
-
-    @property
+    @cached_property
     def columns(self):
         """Column header(s) used by each table."""
 
-        if not hasattr(self, "_columns"):
-            self._columns = []
-            if self.show_id:
-                self._columns.append(self.Reporter.Column("CDR ID"))
-            self._columns.append(self.Reporter.Column("Title"))
-            for _ in range(self.extra):
-                self._columns.append(self.Reporter.Column("", width="50px"))
-        return self._columns
+        columns = []
+        if self.show_id:
+            columns.append(self.Reporter.Column("CDR ID"))
+        columns.append(self.Reporter.Column("Title"))
+        for _ in range(self.extra):
+            columns.append(self.Reporter.Column("", width="50px"))
+        return columns
 
-    @property
+    @cached_property
     def extra(self):
         """How many extra blank columns should be included?"""
 
-        if not hasattr(self, "_extra"):
-            try:
-                self._extra = int(self.fields.getvalue("extra", "0"))
-            except Exception:
-                self._extra = 0
-        return self._extra
+        try:
+            return int(self.fields.getvalue("extra", "0"))
+        except Exception:
+            return 0
 
-    @property
+    @cached_property
     def included(self):
-        """Summaries, modules, or both?"""
+        """Summaries, modules, both, or SVPC?"""
 
-        if not hasattr(self, "_included"):
-            self._included = self.fields.getvalue("included", "s")
-            if self._included not in {i[0] for i in self.INCLUDED}:
-                self.bail()
-        return self._included
+        included = self.fields.getvalue("included", "s")
+        if included not in {i[0] for i in self.INCLUDED}:
+            self.bail()
+        return included
 
-    @property
+    @cached_property
     def language(self):
         """English or Spanish."""
 
-        if not hasattr(self, "_language"):
-            default = self.LANGUAGES[0]
-            self._language = self.fields.getvalue("language", default)
-            if self._language not in self.LANGUAGES:
-                self.bail()
-        return self._language
+        default = self.LANGUAGES[0]
+        language = self.fields.getvalue("language", default)
+        if language not in self.LANGUAGES:
+            self.bail()
+        return language
 
-    @property
+    @cached_property
     def show_all(self):
         """True if we should include summaries which are not publishable."""
         return self.fields.getvalue("show_all") == "Y"
 
-    @property
+    @cached_property
     def show_id(self):
         """True if a column for the CDR document IDs should be included."""
         return self.fields.getvalue("show_id") == "Y"
 
-    @property
+    @cached_property
     def subtitle(self):
         """What to display underneath the main banner."""
 
-        if not hasattr(self, "_subtitle"):
-            if self.request == self.SUBMIT:
-                subtitle = f"PDQ {self.language} {self.audience} Summaries"
-                if self.show_all:
-                    subtitle += " (all)"
-                today = self.started.strftime("%Y-%m-%d")
-                self._subtitle = f"{subtitle} {today}"
-            else:
-                self._subtitle = self.SUBTITLE
-        return self._subtitle
+        if self.request != self.SUBMIT:
+            return self.SUBTITLE
+        subtitle = f"PDQ {self.language} {self.audience} Summaries"
+        if self.show_all:
+            subtitle += " (all)"
+        today = self.started.strftime("%Y-%m-%d")
+        return f"{subtitle} {today}"
 
 
 class Board:
@@ -201,12 +201,14 @@ class Board:
         ("p", "summary", "summaries"),
     )
 
-    def __init__(self, control, doc_id):
+    def __init__(self, control, doc_id=None):
         """Remeber the caller's values.
 
         Pass:
             control - access to the report options and report-building tools
             doc_id - integer for CDR Organization document ID for the board
+                     (will be None if the new SVPC flavor of the report was
+                     requested, as those summaries have no board, so I'm told)
         """
 
         self.__control = control
@@ -216,125 +218,115 @@ class Board:
         """Sort by board name."""
         return self.name < other.name
 
-    @property
+    @cached_property
     def control(self):
         """Access to the report options and report-building tools."""
         return self.__control
 
-    @property
+    @cached_property
     def docs(self):
         """CDR Summary documents for this board."""
 
-        if not hasattr(self, "_docs"):
-            control = self.control
-            query_term = "query_term"
-            if not control.show_all:
-                query_term += "_pub"
-            cols = [
-                "t.doc_id",
-                "t.value AS title",
-                "m.value AS module_only",
-                "s.value as svpc",
-            ]
-            if control.language != "English":
-                cols.append("o.value AS original_title")
-            query = control.Query(f"{query_term} t", *cols).unique()
-            query.join(f"{query_term} a", "a.doc_id = t.doc_id")
-            query.join(f"{query_term} l", "l.doc_id = t.doc_id")
-            query.join("active_doc d", "d.id = t.doc_id")
-            query.where("t.path = '/Summary/SummaryTitle'")
-            query.where("a.path = '/Summary/SummaryMetaData/SummaryAudience'")
-            query.where("l.path = '/Summary/SummaryMetaData/SummaryLanguage'")
-            query.where(query.Condition("a.value", f"{control.audience}s"))
-            query.where(query.Condition("l.value", control.language))
-            if control.language == "English":
-                query.join(f"{query_term} b", "b.doc_id = t.doc_id")
-            else:
-                query.join(f"{query_term} e", "e.doc_id = t.doc_id")
-                query.join(f"{query_term} b", "b.doc_id = e.int_val")
-                query.join(f"{query_term} o", "o.doc_id = e.int_val")
-                query.where("e.path = '/Summary/TranslationOf/@cdr:ref'")
-                query.where("o.path = '/Summary/SummaryTitle'")
-            query.outer(f"{query_term} s", "s.doc_id = t.doc_id",
-                        "s.path = '/Summary/@SVPC'")
+        control = self.control
+        query_term = "query_term"
+        if not control.show_all:
+            query_term += "_pub"
+        cols = [
+            "t.doc_id",
+            "t.value AS title",
+            "m.value AS module_only",
+        ]
+        if control.language != "English":
+            cols.append("o.value AS original_title")
+        query = control.Query(f"{query_term} t", *cols).unique()
+        query.join(f"{query_term} a", "a.doc_id = t.doc_id")
+        query.join(f"{query_term} l", "l.doc_id = t.doc_id")
+        query.join("active_doc d", "d.id = t.doc_id")
+        query.where("t.path = '/Summary/SummaryTitle'")
+        query.where("a.path = '/Summary/SummaryMetaData/SummaryAudience'")
+        query.where("l.path = '/Summary/SummaryMetaData/SummaryLanguage'")
+        query.where(query.Condition("a.value", f"{control.audience}s"))
+        query.where(query.Condition("l.value", control.language))
+        if control.language == "English":
+            query.join(f"{query_term} b", "b.doc_id = t.doc_id")
+        else:
+            query.join(f"{query_term} e", "e.doc_id = t.doc_id")
+            query.join(f"{query_term} b", "b.doc_id = e.int_val")
+            query.join(f"{query_term} o", "o.doc_id = e.int_val")
+            query.where("e.path = '/Summary/TranslationOf/@cdr:ref'")
+            query.where("o.path = '/Summary/SummaryTitle'")
+        if self.control.included == "p":
+            query.join(f"{query_term} s", "s.doc_id = t.doc_id",
+                       "s.path = '/Summary/@SVPC'")
+            query.where("s.value = 'Yes'")
+        else:
             b_path = "/Summary/SummaryMetaData/PDQBoard/Board/@cdr:ref"
             query.where(query.Condition("b.path", b_path))
             query.where(query.Condition("b.int_val", self.id))
-            query.outer(f"{query_term} m", "m.doc_id = t.doc_id",
-                        "m.path = '/Summary/@ModuleOnly'")
-            rows = query.execute(control.cursor).fetchall()
-            self._docs = [Summary(row) for row in rows]
-        return self._docs
+            query.outer(f"{query_term} s", "s.doc_id = t.doc_id",
+                        "s.path = '/Summary/@SVPC'")
+            query.where("s.value IS NULL")
+        query.outer(f"{query_term} m", "m.doc_id = t.doc_id",
+                    "m.path = '/Summary/@ModuleOnly'")
+        rows = query.execute(control.cursor).fetchall()
+        return [Summary(row) for row in rows]
 
-    @property
+    @cached_property
     def id(self):
         """Integer for CDR Organization document ID for the board."""
         return self.__doc_id
 
-    @property
+    @cached_property
     def modules(self):
         """Summary documents which can only be used as modules."""
+        return [doc for doc in self.docs if doc.module]
 
-        if not hasattr(self, "_modules"):
-            self._modules = [doc for doc in self.docs if doc.module]
-        return self._modules
-
-    @property
+    @cached_property
     def name(self):
         """String for the board's brief name."""
 
-        if not hasattr(self, "_name"):
-            self._name = f"{self.control.boards[self.id]} Editorial Board"
-        return self._name
+        if self.id:
+            return f"{self.control.boards[self.id]} Editorial Board"
+        return ""
+
 
     @cached_property
-    def svpc(self):
-        """Which summaries are SVPC summaries?"""
-        return [doc for doc in self.docs if doc.svpc]
-
-    @property
     def summaries(self):
         """Summary documents which can be published independently."""
+        return [doc for doc in self.docs if not doc.module]
 
-        if not hasattr(self, "_summaries"):
-            self._summaries = [doc for doc in self.docs if not doc.module]
-        return self._summaries
-
-    @property
+    @cached_property
     def tables(self):
         """Create the table(s) showing summaries for this board."""
 
-        if not hasattr(self, "_tables"):
-            Cell = self.control.Reporter.Cell
-            Table = self.control.Reporter.Table
-            columns = self.control.columns
-            self._tables = []
-            for included, singular, plural in self.TABLES:
-                if self.control.included in included:
+        Cell = self.control.Reporter.Cell
+        Table = self.control.Reporter.Table
+        columns = self.control.columns
+        tables = []
+        for included, singular, plural in self.TABLES:
+            if self.control.included in included:
+                docs = getattr(self, plural)
+                if docs:
+                    what = singular if len(docs) == 1 else plural
                     if included == "p":
-                        docs = self.svpc
+                        caption = f"{len(docs)} SVPC {what}"
                     else:
-                        docs = getattr(self, plural)
-                    if docs:
-                        what = singular if len(docs) == 1 else plural
-                        if included == "p":
-                            what = f"SVPC {what}"
                         caption = f"{self.name} ({len(docs)} {what})"
-                        rows = []
-                        for doc in sorted(docs):
-                            row = []
-                            if self.control.show_id:
-                                row = [Cell(doc.doc_id, classes="center")]
-                            title = [doc.title]
-                            if doc.original_title:
-                                title.append(f"({doc.original_title})")
-                            row.append(title)
-                            while len(row) < len(columns):
-                                row.append("")
-                            rows.append(row)
-                        opts = dict(caption=caption, columns=columns)
-                        self._tables.append(Table(rows, **opts))
-        return self._tables
+                    rows = []
+                    for doc in sorted(docs):
+                        row = []
+                        if self.control.show_id:
+                            row = [Cell(doc.doc_id, classes="center")]
+                        title = [doc.title or "** NO TITLE **"]
+                        if doc.original_title:
+                            title.append(f"({doc.original_title})")
+                        row.append(title)
+                        while len(row) < len(columns):
+                            row.append("")
+                        rows.append(row)
+                    opts = dict(caption=caption, columns=columns)
+                    tables.append(Table(rows, **opts))
+        return tables
 
 
 class Summary:
@@ -348,25 +340,22 @@ class Summary:
         """Sort by title, case insensitive."""
         return self.key < other.key
 
-    @property
+    @cached_property
     def doc_id(self):
         """Integer for the unique CDR document ID for this PDQ summary."""
         return self.__row.doc_id
 
-    @property
+    @cached_property
     def key(self):
         """Sort by normalized title."""
+        return self.title.lower()
 
-        if not hasattr(self, "_key"):
-            self._key = self.title.lower()
-        return self._key
-
-    @property
+    @cached_property
     def module(self):
         """True if this document can only be used as an included module."""
         return self.__row.module_only == "Yes"
 
-    @property
+    @cached_property
     def original_title(self):
         """Title of the English document of which this is a translation."""
         try:
@@ -375,11 +364,6 @@ class Summary:
             return None
 
     @cached_property
-    def svpc(self):
-        """ True if this summary is an SVPC summary."""
-        return self.__row.svpc == "Yes"
-
-    @property
     def title(self):
         """String for the title of this PDQ summary."""
         return self.__row.title
