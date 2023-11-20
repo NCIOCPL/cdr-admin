6bfa325c5ac6d50e2e35a59e910fdc06fb931278
Bob Kline <bkline@rksystems.com>
2023-11-20 06:43:15 -0500

    Enhance Citations In Summaries report (OCECDR-5293)
    * add columns for summary IDs, titles, and board, and Pubmed IDs
    * convert report to use batch job harness
    * add option for running throttled report locally for testing

--- Inetpub/wwwroot/cgi-bin/cdr/CitationsInSummaries.py
+++ Inetpub/wwwroot/cgi-bin/cdr/CitationsInSummaries.py
@@ -3,44 +3,171 @@
 """ Report all citations linked by a summary document.
 """
 
-from cdrcgi import Controller, Reporter, BASE
-from cdrapi import db
+from functools import cached_property
+from cdrcgi import Controller, Reporter, Excel
+from cdrapi.docs import Doc
 from cdrapi.settings import Tier
+from cdrbatch import CdrBatch
+from CdrLongReports import CitationsInSummaries
 
 
 class Control(Controller):
     TITLE = "Citations In Summaries"
     COLS = (
-        Reporter.Column("CDR-ID", width="60px"),
+        Reporter.Column("Citation ID", width="60px"),
         Reporter.Column("Citation Title", width="1000px"),
+        Reporter.Column("PMID", width="70px"),
+        Reporter.Column("Summary ID", width="60px"),
+        Reporter.Column("Summary Title", width="500px"),
+        Reporter.Column("Summary Boards", width="500px"),
     )
-    HOST = Tier("PROD").hosts["APPC"]
-    URL = f"https://{HOST}{BASE}/QcReport.py?Session=guest&DocId={{:d}}"
-    URL += "&DocVersion=-1"
+    OPTS = dict(columns=COLS, caption=TITLE, sheet_name=TITLE)
+    LINK_PATH = "/Summary/%CitationLink/@cdr:ref"
+    LOGNAME = "CitationsInSummaries"
+    LONG_REPORTS = "lib/Python/CdrLongReports.py"
+    OPTIONS = (
+        ("quick", "Quick sample report"),
+        ("debug", "Log debugging information"),
+    )
+    TIP = "Run report directly instead of queuing a batch report."
+    REPORT_LIMITS = (
+        ("max-citations", "Max Citations", 100),
+        ("max-seconds", "Max Seconds", 60),
+    )
+    INSTRUCTIONS = (
+        "The full report generally takes about an hour or more to generate. "
+        "When the report is ready an email notification will be sent "
+        "to the address(es) provided, with a link to the Excel report. "
+        "For testing you can choose a quick version of the report, "
+        "for which you can specify the maximum number of Citation "
+        "documents to be included, and/or the maximum number of seconds "
+        "for collecting the information to be included in the report."
+    )
+    SCRIPT = """\
+function check_options(setting) {
+    if (setting == "quick") {
+        if (jQuery("#options-quick").prop("checked")) {
+            jQuery("#limit-block").show();
+            jQuery("#email-block").hide();
+        }
+        else {
+            jQuery("#limit-block").hide();
+            jQuery("#email-block").show();
+        }
+    }
+}
+"""
 
-    def run(self):
-        self.show_report()
+    def populate_form(self, page):
+        """Find out how the users wants to run this report.
 
-    def build_tables(self):
-        query = db.Query("active_doc d", "d.id", "d.title").order("d.id DESC")
-        query.join("query_term q", "q.int_val = d.id")
-        query.join("active_doc s", "s.id = q.doc_id")
-        query.where("q.path LIKE '/Summary/%CitationLink/@cdr:ref'")
-        rows = []
-        for doc_id, title in query.unique().execute():
-            url = self.URL.format(doc_id)
-            row = Reporter.Cell(doc_id, center=True, href=url), title.strip()
-            rows.append(row)
+        Required positional argument:
+          page - instance of the cdrcgi.HTMLPage class
+        """
+
+        # Explain how the report works.
+        fieldset = page.fieldset("Instructions")
+        fieldset.append(page.B.P(self.INSTRUCTIONS))
+        page.form.append(fieldset)
+
+        # Add the options which are always visible.
+        fieldset = page.fieldset("Report Options")
+        for value, label in self.OPTIONS:
+            opts = dict(value=value, label=label)
+            if value == "quick":
+                opts["tooltip"] = self.TIP
+            fieldset.append(page.checkbox("options", **opts))
+        page.form.append(fieldset)
+
+        # Add a block for the required email address(s).
+        fieldset = page.fieldset("Email (Required)", id="email-block")
+        opts = dict(value=self.email, label="Address(es)")
+        opts["tooltip"] = "Separate multiple addresses with a space."
+        fieldset.append(page.text_field("email", **opts))
+        page.form.append(fieldset)
+
+        # Add a second block for options on the "quick" report.
+        fieldset = page.fieldset("Report Limits", id="limit-block")
+        fieldset.set("class", "hidden")
+        for name, display, value in self.REPORT_LIMITS:
+            opts = dict(label=display, value=value)
+            fieldset.append(page.text_field(name, **opts))
+        page.form.append(fieldset)
+        page.add_script(self.SCRIPT)
+
+    def show_report(self):
+        """Queue the report or generate it directly."""
+
+        args = []
+        if self.debug:
+            args.append(("debug", True))
+        if self.quick:
+            args.append(("max_time", self.max_seconds))
+            args.append(("limit", self.max_citations))
+            args.append(("throttle", True))
         opts = dict(
-            columns=self.COLS,
-            caption=self.TITLE,
-            sheet_name=self.TITLE,
+            jobName=CitationsInSummaries.NAME,
+            command=self.LONG_REPORTS,
+            email=self.email,
+            args=args,
         )
-        return Reporter.Table(rows, **opts)
+        job = CdrBatch(**opts)
+        if self.quick:
+            report = CitationsInSummaries(job)
+            report.excel = Excel(wrap=True, stamp=True)
+            report.add_sheets()
+            self.logger.info("elapsed time for report: %s", self.elapsed)
+            report.excel.send()
+        else:
+            if not self.email:
+                self.bail("Missing required email address.")
+            try:
+                job.queue()
+            except Exception as e:
+                self.logger.exception("Failure queuing job")
+                self.bail(f"Unable to queue job: {e}")
+            args = self.TITLE, self.subtitle, self.script, self.SUBMENU
+            job.show_status_page(self.session, *args)
+            self.logger.info("elapsed time for report: %s", self.elapsed)
 
-    @property
-    def format(self):
-        return "excel"
+    @cached_property
+    def debug(self):
+        """True if the debugging option has been checked."""
+        return True if "debug" in self.options else False
+
+    @cached_property
+    def email(self):
+        """Where to send notification if the report is queued."""
+
+        addresses = self.fields.getvalue("email", "").strip()
+        return addresses or self.session.user.email
+
+    @cached_property
+    def loglevel(self):
+        """Bump up logging verbosity if requested."""
+        return "DEBUG" if self.debug else "INFO"
+
+    @cached_property
+    def max_citations(self):
+        """When to stop when testing."""
+        return int(self.fields.getvalue("max-citations", "10"))
+
+    @cached_property
+    def max_seconds(self):
+        """When to stop when testing."""
+        return int(self.fields.getvalue("max-seconds", "10"))
+
+    @cached_property
+    def options(self):
+        """Checkbox options for the report."""
+        return self.fields.getlist("options")
+
+    @cached_property
+    def quick(self):
+        """True if we deliver the report immediately, avoiding the queue."""
+        return "quick" in self.options
 
 
-Control().run()
+if __name__ == "__main__":
+    """Don't run if loaded as a module."""
+    Control().run()
